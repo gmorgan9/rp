6e04559ae86e655aa2b6dbdb1fd4f52459d4b02f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-28 15:01:56 -0700

    update

---
+++ app/includes/sidebar.php
@@ -0,0 +1,76 @@
+<nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+    <div class="position-sticky">
+        <div class="list-group list-group-flush mx-3 mt-4">
+            <a href="<?php //echo BASE_URL . '/pages/dashboard.php' ?>" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+                <i class="bi bi-sliders2"></i>
+                <span>  Dashboard</span>
+            </a>
+            <?php //if($_SESSION['acc_type'] == 0) { ?>
+            <a href="<?php //echo BASE_URL . '/pages/class-schedule.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
+                <i class="bi bi-calendar-range"></i>
+                <span>  Schedule</span>
+            </a>
+            <a href="<?php //echo BASE_URL . '/pages/assignments.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
+                <i class="bi bi-journal-bookmark"></i>
+                <span>  Assignments</span>
+            </a>
+            <?php //} ?>
+
+            <?php //if($_SESSION['acc_type'] == 1){ ?>
+                <br>
+                <span style="margin-left: 38px; margin-bottom: -10px;">Admin Links</span>
+                <hr>
+                <a style="margin-top: -15px;" href="<?php //echo BASE_URL . '/admin/students.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
+                    <i class="bi bi-person-badge"></i>
+                    <span>  Students</span>
+                </a>
+                <?php 
+                ////WHERE approval_status = 'pending' OR approval_status = 'terminated' 
+                //$sql = " SELECT * FROM course WHERE approval_status = 'pending' OR approval_status = 'terminated'";
+               // if ($result = mysqli_query($conn, $sql)) {
+               //     $rowcount = mysqli_num_rows( $result );
+                ?>
+                <a href="<?php //echo BASE_URL . '/admin/courses.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
+                    <i class="bi bi-briefcase"></i>
+                    <?php //if($rowcount == 0){ ?>
+                        <span>  Courses</span>
+                    <?php //} else { ?>
+                        <span>  Courses</span> &nbsp;  <span class="badge rounded-pill text-bg-danger" style="margin-top: -10px !important;"><?php echo $rowcount; ?></span>
+                    <?php //} ?>
+                </a>
+                <?php 
+                //// WHERE approval_status = 'pending' OR approval_status = 'terminated' 
+                //$sql = " SELECT * FROM assignment";
+                //if ($result = mysqli_query($conn, $sql)) {
+                //    $rowcount = mysqli_num_rows( $result );
+                ?>
+                <a href="<?php //echo BASE_URL . '/pages/assignments.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
+                    <i class="bi bi-calendar-range"></i>
+                    <?php //if($rowcount == 0){ ?>
+                        <span>  Assignments</span>
+                    <?php //} else { ?>
+                        <span>  Assignments</span> &nbsp;  <span class="badge rounded-pill text-bg-danger" style="margin-top: -10px !important;"><?php echo $rowcount; ?></span>
+                    <?php //}} ?>
+                </a>
+                <a href="<?php //echo BASE_URL . '/admin/semester.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
+                    <i class="bi bi-calendar3"></i>
+                    <span>  Semester</span>
+                </a>
+                <a href="<?php //echo BASE_URL . '/admin/reports.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
+                    <i class="bi bi-bar-chart"></i>
+                    <span>  Reports</span>
+                </a>
+                <?php //} ?>
+            <?php //} else {?>
+                <br>
+                <span style="margin-left: 38px; margin-bottom: -10px;">Student Links</span>
+                <hr>
+                <a style="margin-top: -15px;" href="<?php //echo BASE_URL . '/pages/course-request.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
+                    <i class="bi bi-briefcase"></i>
+                    <span>  Request a Course</span>
+                </a>
+
+           <?php //} ?>
+        </div>
+    </div>
+</nav>
