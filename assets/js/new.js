38914dd35ed2a7d97f79a62fc649386c9e953118
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 13:25:25 -0700

    update

---
+++ assets/js/new.js
@@ -0,0 +1,7 @@
+function dodajAktywne(elem) {
+    var a = document.getElementsByTagName('a')
+    for (i = 0; i < a.length; i++) {
+        a[i].classList.remove('active')
+    }
+    elem.classList.add('active');
+}
