aaf2a05022c6067e6a249efe91cf4d8cfad7852e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 12:03:41 -0700

    update

--- app/database/connection.php
+++ app/database/connection.php
@@ -14,11 +14,7 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 } else {
     echo "
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    <br><br><br><br>
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     connection successful";
