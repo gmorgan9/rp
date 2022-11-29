9efb66082ba9a07e9bc7dd74543752003cf84fc0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-28 23:22:26 -0700

    update

---
+++ app/includes/footer.php
@@ -0,0 +1,15 @@
+<!-- WORKING -->
+<?php //if(isset($_SESSION['fname'])){ ?>
+    <footer class="fixed-bottom py-3" style="background-color: white">
+        <div class="text-center text-muted" style="background-color: white;">
+<?php //} else { ?>
+    <!-- <footer class="fixed-bottom py-3" style="background-color: #17181C;"> -->
+        <!-- <div class="text-center text-muted" style="background-color: rgba(0, 0, 0, 0.05);"> -->
+<?php //}?>
+
+<?php 
+$year = date("Y"); 
+?>
+        &copy; <?php echo $year; ?> CacheUp
+    </div>
+</footer>
