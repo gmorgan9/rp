08068979e0e7b6193710767561fd001338489322
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 16:51:05 -0700

    update

--- index.php
+++ index.php
@@ -34,7 +34,6 @@ require_once "path.php";
 <body>
     
 <div class="main-container">
-<?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
 <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
 
 <?php include(ROOT_PATH . "/app/includes/sidebar.php") ?>
@@ -58,9 +57,7 @@ require_once "path.php";
   })
 </script>
     
-
-
-<!-- <script src="assets/js/new.js"></script> -->
+<?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
 
     <script src="assets/js/dropdown.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
