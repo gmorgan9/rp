4d6dbda6018982bfb129137118cbcfc3ea050eeb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-28 21:06:24 -0700

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -8,7 +8,7 @@
         
             <div class="mx-auto">
             <div class="dropdown-center">
-                <button class="dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
+                <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
                     POSTS
                 </button>
                 <ul class="dropdown-menu">
