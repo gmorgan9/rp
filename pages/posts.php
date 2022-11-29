69c1af315950fb553da3442d643b757915a98fc3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 16:27:12 -0700

    update

---
+++ pages/posts.php
@@ -0,0 +1,72 @@
+<?php
+
+require_once "../app/database/connection.php";
+// require_once "app/database/functions.php";
+require_once "../path.php";
+// session_start();
+
+// if(isLoggedIn()){
+//   header('location: '. BASE_URL . '/pages/dashboard.php');
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+
+    <link rel="stylesheet" href="../assets/styles.css?v=1.81">
+
+    <title>Dashboard - CacheUp Blog</title>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+<div class="main-container">
+<?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+
+<?php include(ROOT_PATH . "/app/includes/sidebar.php") ?>
+        
+<div class="main">
+  <div class="page-header mx-auto">
+    <p class="page_title" style="float: left; padding-top: 2px;">Posts</p>
+    <!-- <ul class="breadcrumb">
+      <li>Dashboard</li>
+    </ul> -->
+
+    testing
+  </div>
+</div>
+</div>
+
+    
+<?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+
+<!-- <script src="../assets/js/new.js"></script> -->
+
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script type="text/javascript">
+  $(document).on('click', 'a', function() {
+    $(this).addClass('active').siblings().removeClass('active')
+  })
+  $(document).on('click', 'ul li a', function() {
+    $(this).removeClass('active')
+  })
+</script>
+
+
+    <script src="../assets/js/dropdown.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
