1115c6bc9fc8e044416928cbc39035947ed9551a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-28 21:17:33 -0700

    update

--- index.php
+++ index.php
@@ -22,6 +22,9 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"crossorigin="anonymous"></script>
+
 
     <link rel="stylesheet" href="assets/styles.css">
 
@@ -40,7 +43,7 @@
     </p>
 
 
-    <script src="assets/js/dropdown.js"></script>
+    <!-- <script src="assets/js/dropdown.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
