eb9c64a2d0f2b2c1162a46f6f878a27109f69c59
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 13:11:00 -0700

    update

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ require_once "path.php";
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 
-    <link rel="stylesheet" href="assets/styles.css?v=1.24">
+    <link rel="stylesheet" href="assets/styles.css?v=1.25">
 
     <title>CacheUp Blog</title>
 
