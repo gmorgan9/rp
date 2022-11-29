1f36ec9bd65726e61be45c52e04a075eac6be1e0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 12:01:18 -0700

    update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-// require_once "app/database/connection.php";
+include(ROOT_PATH . "/app/database/connection.php");
 // require_once "app/database/functions.php";
 require_once "path.php";
 // session_start();
