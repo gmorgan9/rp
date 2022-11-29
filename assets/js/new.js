5077e8a7d8d4a4710214aede2399605e774b1a2d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 13:23:07 -0700

    update

--- assets/js/new.js
+++
@@ -1,7 +0,0 @@
-const boxes = document.querySelectorAll('.a1');
-
-for (const box of boxes) {
-  box.addEventListener('click', function handleClick() {
-    box.classList.add('bg-yellow');
-  });
-}
