3e6ea7b3e5246208c01230119a2c027a4ede1dcc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-28 19:30:48 -0700

    update

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -6,7 +6,7 @@ for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
+    if (dropdownContent.style.display === "flex") {
       dropdownContent.style.display = "none";
     } else {
       dropdownContent.style.display = "flex";
