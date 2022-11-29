d0255945df5c7a366312d772813fa923074edc97
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-28 22:22:06 -0700

    update

--- index.php
+++ index.php
@@ -26,10 +26,6 @@
     <link rel="stylesheet" href="assets/styles.css">
 
     <title>CacheUp Blog</title>
-
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
 </head>
 <body>
     
@@ -46,7 +42,7 @@
 
     
 
-    <script src="assets/js/dropdown.js"></script>
+    <!-- <script src="assets/js/dropdown.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
