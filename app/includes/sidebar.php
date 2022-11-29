3373e98c9530c83b3a85bfe601d53f82db9ae8f3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-28 20:02:03 -0700

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -8,7 +8,7 @@
         
             <div class="mx-auto">
             <div class="dropdown-center">
-                <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="true">
+                <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
                     POSTS
                 </button>
                 <ul class="dropdown-menu">
@@ -18,7 +18,6 @@
                 </ul>
             </div>
             </div>
-        
 
             
             <?php //if($_SESSION['acc_type'] == 0) { ?>
