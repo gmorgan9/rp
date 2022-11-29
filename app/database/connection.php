8d6e389bfd6c8e7d6d363fe730e6a1c8fdedcb6a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 11:59:45 -0700

    update

---
+++ app/database/connection.php
@@ -0,0 +1,16 @@
+<!-- WORKING -->
+<?php
+
+$servername = "localhost";
+$username = "garrett";
+$password = "BIGmorgan1999!";
+$database = "cacheup";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+?>
