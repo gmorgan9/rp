3fd7955e36d905b0891b095f9383257ffda7edee
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 16:13:56 -0700

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -55,7 +55,7 @@ require_once "../path.php";
 
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script type="text/javascript">
-  $(document).on('click', 'nav', function() {
+  $(document).on('click', 'nav a', function() {
     $(this).addClass('active').siblings().removeClass('active')
   })
   
