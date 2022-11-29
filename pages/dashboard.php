f19c4a6563ccad57703cd35809587519749e91fa
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 16:11:59 -0700

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -23,7 +23,7 @@ require_once "../path.php";
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 
-    <link rel="stylesheet" href="../assets/styles.css?v=1.75">
+    <link rel="stylesheet" href="../assets/styles.css?v=1.76">
 
     <title>Dashboard - CacheUp Blog</title>
 
@@ -55,7 +55,7 @@ require_once "../path.php";
 
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script type="text/javascript">
-  $(document).on('click', 'p a', function() {
+  $(document).on('click', 'p', function() {
     $(this).addClass('active').siblings().removeClass('active')
   })
   
