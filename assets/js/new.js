3f0f43509eeeb6f9fd138b2ca8ea3a3f71732733
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 13:27:08 -0700

    up

--- assets/js/new.js
+++ assets/js/new.js
@@ -1,6 +1,6 @@
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
+  if (ev.target.tagName === 'a') {
     ev.target.classList.toggle('checked');
   }
 }, false);
