22c4f5fe031bfd78eb8c8467c8c074bb7088bc76
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 12:49:37 -0700

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -1,11 +1,10 @@
 <div id="sidebarMenu" class="d-lg-block bg-white sidebar">
     
 <div class="position-sticky">
-<div class="logo-block" >
         <a style="background-color: #0B4F6C; height: 150px;" class="navbar-brand" href="/">
             <img src="/assets/images/white-logo.png" width="230px" class="text-center logo" alt="">
         </a>
-    </div>
+    
         <div class="list-group list-group-flush mx-3 mt-4">
 
 			<div class="accordion mb-2" id="accordionExample">
