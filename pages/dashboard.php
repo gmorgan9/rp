71722e8d9230ea7a4fc30ab882f509219c3128ef
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 12:29:00 -0700

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -23,7 +23,7 @@ require_once "../path.php";
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 
-    <link rel="stylesheet" href="../assets/styles.css?v=1.11">
+    <link rel="stylesheet" href="../assets/styles.css?v=1.12">
 
     <title>Dashboard - CacheUp Blog</title>
 
