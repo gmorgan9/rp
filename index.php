34f9d128d7d64842bb53c709fe75b37f7142bcc4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 18:16:15 -0700

    update

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ require_once "path.php";
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 
-    <link rel="stylesheet" href="assets/blog.css?v=2.44">
+    <link rel="stylesheet" href="assets/blog.css?v=2.45">
 
     <title>CacheUp Blog</title>
 
@@ -37,7 +37,7 @@ require_once "path.php";
         
 <div class="main">
 
-
+<!-- start header -->
   <div class="page-header">
     <!-- <p class="page_title" style="float: left; padding-top: 2px;">&nbsp;&nbsp;<i class="bi bi-house-door-fill"></i> Home &nbsp;&nbsp; | &nbsp;&nbsp; VIEW POST &nbsp;&nbsp; | &nbsp;&nbsp; NEW</p> -->
     <div class="left">
@@ -53,6 +53,23 @@ require_once "path.php";
         </a>
       </button>
     </div>
+<!-- end header -->
+
+
+<!-- start middle -->
+
+<div class="middle">
+  <h1 class="behind">
+    Home
+  </h1>
+  <h1 class="front">
+    Home
+  </h1>
+</div>
+
+<!-- end middle -->
+
+
     
   </div>
 </div>
