93bfa252c50068dbb39ea86855371273da35d0c6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 10:47:13 -0700

    update

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -4,4 +4,9 @@
         <?php $year = date("Y"); ?>
         &copy; <?php echo $year; ?> CacheUp
     </div>
+    <div class="text-right">
+    <i class="bi bi-linkedin"></i>
+    <i class="bi bi-email"></i>
+    <i class="bi bi-facebook"></i>
+    </div>
 </footer>
