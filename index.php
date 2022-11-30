5c72fbbb3babf2d3c268c324b9b4d940b1b43d87
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 17:34:06 -0700

    update

--- index.php
+++ index.php
@@ -36,8 +36,11 @@ require_once "path.php";
 <div class="main-container">
         
 <div class="main">
+
+
   <div class="page-header">
-    <p class="page_title" style="float: left; padding-top: 2px;">&nbsp;&nbsp;<i class="bi bi-house-door-fill"></i> Home &nbsp;&nbsp; | &nbsp;&nbsp; VIEW POST &nbsp;&nbsp; | &nbsp;&nbsp; NEW</p>
+    <!-- <p class="page_title" style="float: left; padding-top: 2px;">&nbsp;&nbsp;<i class="bi bi-house-door-fill"></i> Home &nbsp;&nbsp; | &nbsp;&nbsp; VIEW POST &nbsp;&nbsp; | &nbsp;&nbsp; NEW</p> -->
+    <img src="/assets/images/white-logo.png" width="230px" class="text-center logo" alt="">
     
   </div>
 </div>
