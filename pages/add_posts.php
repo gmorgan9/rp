a734747d91d198d6a000e5f495724e9a60e9e75d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 14:30:20 -0700

    udpate

--- pages/add_posts.php
+++ pages/add_posts.php
@@ -108,7 +108,7 @@ if (mysqli_num_rows($result) > 0) {
         <div>
             <label>Category</label>
             <select name="category" class="form-control">
-                <option value=""></option>
+                <option value="" class="text-muted">Select one...</option>
                 <?php
                 $query ="SELECT * FROM categories";
                 $result = $conn->query($query);
