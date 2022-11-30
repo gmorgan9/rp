7a50275a9bb73a55297243af1fc832e639ded5e2
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 20:57:36 -0700

    update

--- index.php
+++ index.php
@@ -54,13 +54,13 @@ require_once "path.php";
       </button>
 
       <div class="dropdown">
-        <a class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
-        &nbsp;&nbsp;&nbsp;
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-          <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-        </svg>
-</a>
+        <a class="dropdown-toggle text-decoration-none" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+          &nbsp;&nbsp;&nbsp;
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+          </svg>
+        </a>
   <form class="dropdown-menu p-4">
     <div class="mb-3">
       <label for="exampleDropdownFormEmail2" class="form-label">Email address</label>
