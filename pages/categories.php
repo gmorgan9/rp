e3bab05ca10ffd865a89260269c5839c7972b945
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 14:33:33 -0700

    update

--- pages/categories.php
+++ pages/categories.php
@@ -97,7 +97,7 @@ if(isset($_POST['add-category'])){
     $lname = $row['lname']; 
     $employeeID = $row['idno']?>
     <?php }} ?>
-    <div class="form-group pt-3 mx-auto" style="width: 95%;">
+    <div class="form-group pt-3 mx-auto" style="width: 95%; border-radius: 15px;">
       <label for="category" style="font-size: 14px;">Category <span class="text-muted" style="font-size: 10px;">e.g. "Cybersecurity"</span></label>
       <input class="form-control" id="category" type="text" name="category" value="" required>
     </div>
