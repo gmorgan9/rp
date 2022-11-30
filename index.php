ee37389f54151b0dc6c7ec50a59ea621866c5673
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 21:51:34 -0700

    update

--- index.php
+++ index.php
@@ -165,10 +165,10 @@ if(isset($_POST['register'])){
 
             <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                 <li class="nav-item text-center">
-                  <a class="nav-link active btl" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Login</a>
+                  <a class="btn btn-dark btn-block active btl" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Login</a>
                 </li>
                 <li class="nav-item text-center">
-                  <a class="nav-link btr" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Signup</a>
+                  <a class="btn btn-dark btn-block btr" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Signup</a>
                 </li>
                
               </ul>
