99b7c1a47ee0687d5547be2c3c9d68f915a094ee
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 20:03:57 -0700

    update

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ require_once "path.php";
         </a>
       </button>
       <a href="" class="text-decoration-none text-white pt-5">
-        <i class="bi bi-person-circle" style="font-size: 32px;"></i>
+        <i class="bi bi-person-circle" style="font-size: 32px; margin-top: 15px;"></i>
       </a>
     </div>
 <!-- end header -->
