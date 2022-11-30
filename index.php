b8a0d134e819dad630f980bc73e3ed1ae0684dca
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 10:32:11 -0700

    update

--- index.php
+++ index.php
@@ -334,7 +334,7 @@ if ($loggedin == 1) {
 <!-- social links -->
 
 <!-- Linkedin -->
-<div class="social-links text-center mt-5">
+<div class="social-links text-center mt-7">
   <a class="btn btn-primary" style="background-color: #0082ca; border:none !important;" href="#!" role="button"
     ><i class="bi bi-linkedin"></i>
   </a>
