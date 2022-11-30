85f588e44e166430785645e7fd947a5eba875450
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 22:34:32 -0700

    update

--- index.php
+++ index.php
@@ -166,7 +166,7 @@ if ($loggedin == 1) {
 
 <ul class="list-group list-group-flush" style="width: 250px;">
   <li class="list-group-item fw-bold text-capitalize mb-3" style="border-bottom: none;"><?php echo $firstname; ?></li>
-  <li class="list-group-item w-25"><a href="<?php echo BASE_URL . '/pages/dashboard.php' ?>" class="text-decoration-none text-black">Dashboard</a></li>
+  <li class="list-group-item w-50"><a href="<?php echo BASE_URL . '/pages/dashboard.php' ?>" class="text-decoration-none text-black">Dashboard</a></li>
   <li class="list-group-item"><a href="<?php echo BASE_URL . '/logout.php' ?>" class="text-decoration-none text-black">Logout</a></li>
 </ul>
 
