61a3341685c05d0ce8e887dbb777ac74f0092798
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 17:52:16 -0700

    update

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,6 +1,6 @@
 <!-- WORKING -->
 <footer>
-    <div class="text-center text-muted pt-1" style="font-size: 10px; background-color: white;">
+    <div class="text-center text-muted pt-1" style="font-size: 10px;">
         <?php $year = date("Y"); ?>
         &copy; <?php echo $year; ?> CacheUp
     </div>
