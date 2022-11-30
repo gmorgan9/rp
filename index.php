0da62f673dc2633e9d3c5f7f4ac3b2263ba34159
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 21:31:02 -0700

    update

--- index.php
+++ index.php
@@ -162,6 +162,42 @@ if(isset($_POST['register'])){
             </div>
             <button type="submit" class="btn btn-primary">Sign in</button> -->
 
+
+            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                <li class="nav-item text-center">
+                  <a class="nav-link active btl" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Login</a>
+                </li>
+                <li class="nav-item text-center">
+                  <a class="nav-link btr" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Signup</a>
+                </li>
+               
+              </ul>
+
+              <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                  <div class="form px-4 pt-5">
+                    <input type="text" name="" class="form-control" placeholder="Email or Phone">
+                    <input type="text" name="" class="form-control" placeholder="Password">
+                    <button class="btn btn-dark btn-block">Login</button>
+                </div>
+              </div>
+
+              <!-- REGISTER -->
+              <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                <div class="form px-4">
+                  <input type="text" name="" class="form-control" placeholder="Name">
+                  <input type="text" name="" class="form-control" placeholder="Email">
+                  <input type="text" name="" class="form-control" placeholder="Phone">
+                  <input type="text" name="" class="form-control" placeholder="Password">
+                  <button class="btn btn-dark btn-block">Signup</button>
+                </div>
+              </div>
+
+
+
+
+
+
             <input type="text" name="firstname" required placeholder="enter your first name">
       <input type="text" name="lastname" required placeholder="enter your last name">
       <input type="text" name="username" required placeholder="enter your user name">
