d1f4caf27a5338923573dd4fd4e9c5708588e543
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 21:00:05 -0700

    update

--- index.php
+++ index.php
@@ -59,6 +59,7 @@ require_once "path.php";
             <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
           </svg>
           </a>
+          <br><br>
           <form class="dropdown-menu p-4">
             <div class="mb-3">
               <label for="exampleDropdownFormEmail2" class="form-label">Email address</label>
