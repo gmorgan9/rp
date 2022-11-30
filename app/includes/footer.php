a8feaaac26f50cc772386af8536e3d49490e1cc7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 10:49:18 -0700

    update

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -4,11 +4,4 @@
         <?php $year = date("Y"); ?>
         &copy; <?php echo $year; ?> CacheUp
     </div>
-    <div class="text-right">
-    <a class="btn btn-primary" style="background-color: #3b5998; border:none !important;" href="#!" role="button">
-    <i class="bi bi-facebook"></i>
-  </a>
-    <i class="bi bi-email"></i>
-    <i class="bi bi-facebook"></i>
-    </div>
 </footer>
