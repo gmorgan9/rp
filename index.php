5ce409bceaa573c88b4201747cdb9f198ef3a246
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-29 19:45:40 -0700

    update

--- index.php
+++ index.php
@@ -68,6 +68,44 @@ require_once "path.php";
 
 <!-- end middle -->
 
+
+
+<!-- start blog posts -->
+
+<div class="mx-auto pop-post row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-175">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-175">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a short card.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-175">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- end blog posts -->
+
+
+
+
+
+
 <div class="sub-title">
   <h1 class="behind-2 mt-5">
     Popular Posts
