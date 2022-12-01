af7512e1de2bd9171370477cd7d0fc0a513c34fe
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 19:36:42 -0700

    update

--- single_post.php
+++ single_post.php
@@ -245,7 +245,7 @@ if (mysqli_num_rows($result) > 0) {
 
 ?>
 
-<div class="blog_post" style="width: 75%; background-color: #1f1f1f;">
+<div class="blog_post" style="width: 65%; background-color: #1f1f1f;">
 <?php echo $row['title']; ?>
 </div>
 
