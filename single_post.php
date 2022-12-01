215da99d17c28be4388cf1cdf659548572854974
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 19:57:13 -0700

    update

--- single_post.php
+++ single_post.php
@@ -260,7 +260,7 @@ if (mysqli_num_rows($result) > 0) {
         <?php echo $row['title']; ?>
     </h1>
 
-    <nav class="mt-5" aria-label="breadcrumb" style="--bs-breadcrumb-divider: '>'; breadcrumb-divider-color: white;">
+    <nav class="mt-4" aria-label="breadcrumb" style="--bs-breadcrumb-divider: '>'; breadcrumb-divider-color: white;">
       <ol class="breadcrumb">
         <li class="breadcrumb-item" style="font-size: 12px;"><a href="#" class="text-decoration-none text-uppercase" style="color: #073C53 !important;">Home</a></li>
         <li class="breadcrumb-item" style="font-size: 12px;"><a href="#" class="text-decoration-none text-uppercase" style="color: #073C53 !important;">Library</a></li>
