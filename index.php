a16529c91a31e5d55d77560a00691c6c4e78eef9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 20:28:20 -0700

    update

--- index.php
+++ index.php
@@ -252,7 +252,7 @@ if ($loggedin == 1) {
 
 <div class="mx-auto pop-post row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
-    <div class="card" style="height: 350px;">
+    <div class="card" style="height: 350px; background-color: #1f1f1f;">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -260,7 +260,7 @@ if ($loggedin == 1) {
     </div>
   </div>
   <div class="col">
-    <div class="card" style="height: 350px;">
+    <div class="card" style="height: 350px; background-color: #1f1f1f;">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a short card.</p>
@@ -268,7 +268,7 @@ if ($loggedin == 1) {
     </div>
   </div>
   <div class="col">
-    <div class="card" style="height: 350px;">
+    <div class="card" style="height: 350px; background-color: #1f1f1f;">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
