996bf776311df6104a940357c76291f3bcab7c6b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 21:17:34 -0700

    update

--- pages/categories.php
+++ pages/categories.php
@@ -82,7 +82,7 @@ if(isset($_POST['add-category'])){
             <!-- START ADD COMPANY (LEFT SIDE) -->
     <div class="page-content" style=" width: 32%; height: 175px !important; border-radius: 15px;">
     <form action="" method="post">
-    <div class="section-header pt-2">
+    <div class="section-header pt-2" style="background-color: #1f1f1f;">
       <span class="text-muted pt-4" style="width: 95%;">New Category</span>
     </div>
     <hr style="margin-bottom: -5px; margin-top: 5px;">
@@ -97,7 +97,7 @@ if(isset($_POST['add-category'])){
     $lname = $row['lname']; 
     $employeeID = $row['idno']?>
     <?php }} ?>
-    <div class="form-group pt-3 mx-auto" style="width: 95%; background-color: #1f1f1f;">
+    <div class="form-group pt-3 mx-auto" style="width: 95%;">
       <label class="text-black" for="category" style="font-size: 14px;">Category <span class="text-muted" style="font-size: 10px;">e.g. "Cybersecurity"</span></label>
       <input class="form-control" id="category" type="text" name="category" value="" required>
     </div>
