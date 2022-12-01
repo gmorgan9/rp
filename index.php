ef92b053adfb15dde40286b6060d395dffd00700
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 20:43:59 -0700

    update

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ if(isset($_POST['register'])){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 
-    <link rel="stylesheet" href="assets/blog.css?v=3.77">
+    <link rel="stylesheet" href="assets/blog.css?v=3.78">
 
     <title>CacheUp Blog</title>
 
