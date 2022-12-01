779915ea5d1fd5b1485e4c5abd8b155713c8c209
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-01 14:48:43 -0700

    update

--- pages/actions/edit_post.php
+++ pages/actions/edit_post.php
@@ -2,7 +2,7 @@
 
 require_once "../../app/database/connection.php";
 // require_once "app/database/functions.php";
-// require_once "../../path.php";
+require_once "../../path.php";
 session_start();
 
 // if(isLoggedIn()){
@@ -52,7 +52,7 @@ if(isset($_POST['submit'])){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 
-    <link rel="stylesheet" href="../assets/styles.css?v=2.50">
+    <link rel="stylesheet" href="../../assets/styles.css?v=2.50">
 
     <script src="https://cdn.tiny.cloud/1/7kainuaawjddfzf3pj7t2fm3qdjgq5smjfjtsw3l4kqfd1h4/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
