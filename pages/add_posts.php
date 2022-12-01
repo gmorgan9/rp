4ce7a3592a6993a5e3d999a97193c819e7d238d9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 22:49:04 -0700

    update

--- pages/add_posts.php
+++ pages/add_posts.php
@@ -100,6 +100,30 @@ if (mysqli_num_rows($result) > 0) {
         <h3>
             New Post
         </h3>
+        <div class="modal_help float-end">
+
+          <div class="modal" tabindex="-1">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Modal title</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+
+        </div>
         <div>
             <label>Post Title</label>
             <input class="form-control" style="width: 99%;" type="text" name="title">
