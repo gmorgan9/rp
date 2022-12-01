00933eaddc31cd7d55f3a3bf8f2dc5d7c22eac7b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 19:54:19 -0700

    update

--- single_post.php
+++ single_post.php
@@ -249,9 +249,9 @@ if (mysqli_num_rows($result) > 0) {
 
     <nav aria-label="breadcrumb" style="--bs-breadcrumb-divider: '>'; breadcrumb-divider-color: white;">
       <ol class="breadcrumb">
-        <li class="breadcrumb-item" style="font-size: 12px;"><a href="#" class="text-decoration-none" style="color: #073C53 !important;">Home</a></li>
-        <li class="breadcrumb-item" style="font-size: 12px;"><a href="#" class="text-decoration-none" style="color: #073C53 !important;">Library</a></li>
-        <li class="breadcrumb-item active text-white" aria-current="page" style="font-size: 12px;">Data</li>
+        <li class="breadcrumb-item" style="font-size: 12px;"><a href="#" class="text-decoration-none text-uppercase" style="color: #073C53 !important;">Home</a></li>
+        <li class="breadcrumb-item" style="font-size: 12px;"><a href="#" class="text-decoration-none text-uppercase" style="color: #073C53 !important;">Library</a></li>
+        <li class="breadcrumb-item active text-white text-uppercase" aria-current="page" style="font-size: 12px;">Data</li>
       </ol>
     </nav>
 
