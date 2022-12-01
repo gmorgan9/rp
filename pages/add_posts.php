9b508045fb2ea9ccad1c70d9b5986658b83e6322
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-11-30 22:58:45 -0700

    update

--- pages/add_posts.php
+++ pages/add_posts.php
@@ -109,7 +109,7 @@ if (mysqli_num_rows($result) > 0) {
 
             <!-- Modal -->
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
+              <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                   <div class="modal-header">
                     <h1 class="modal-title fs-5 text-black" id="exampleModalLabel">Instructions</h1>
