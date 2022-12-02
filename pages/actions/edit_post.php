4c0818ad24b8e1703c996b924b06c3a24c034eab
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-01 20:54:34 -0700

    update

--- pages/actions/edit_post.php
+++ pages/actions/edit_post.php
@@ -149,7 +149,7 @@ if (mysqli_num_rows($result) > 0) {
         </div>
         <div>
             <label>Post Title</label>
-            <input class="form-control" style="width: 99%;" type="text" value="<?php echo $row['title']; ?>">
+            <input class="form-control" style="width: 99%;" type="text" name="title" value="<?php echo $row['title']; ?>">
         </div>
         <div class="pt-3"></div>
         <div>
