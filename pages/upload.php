2fa7ff944fc953bc9c245b6d0ec54858bf98327a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-12 19:42:39 -0700

    update

--- pages/upload.php
+++ pages/upload.php
@@ -7,13 +7,13 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
+//   if($check !== false) {
     echo "File is an image.";
     $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
+//   } else {
+//     echo "File is not an image.";
+//     $uploadOk = 0;
+//   }
 }
 
 // Check if file already exists
