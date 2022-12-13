3591fd0c0f9ed80802ef4911c33cf096a86c9508
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-12 19:49:51 -0700

    update

--- pages/upload.php
+++ pages/upload.php
@@ -1,49 +1,49 @@
 <?php
-$target_dir = "/upload/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-//   if($check !== false) {
-    echo "File is an image.";
-    $uploadOk = 1;
-//   } else {
-//     echo "File is not an image.";
-//     $uploadOk = 0;
-//   }
+error_reporting(0);
+
+?>
+
+<?php
+
+$msg = ""; 
+
+// check if the user has clicked the button "UPLOAD" 
+
+if (isset($_POST['submit'])) {
+
+    $filename = $_FILES["choosefile"]["name"];
+
+    $tempname = $_FILES["choosefile"]["tmp_name"];  
+
+        $folder = "upload/".$filename;   
+
+    // connect with the database
+
+    $db = mysqli_connect("localhost", "garrett", "BIGmorgan1999!", "cacheup"); 
+
+        // query to insert the submitted data
+
+        $sql = "UPDATE users SET filename = '$filename'";
+
+        // function to execute above query
+
+        mysqli_query($db, $sql);       
+
+        // Add the image to the "image" folder"
+
+        if (move_uploaded_file($tempname, $folder)) {
+
+            $msg = "Image uploaded successfully";
+
+        }else{
+
+            $msg = "Failed to upload image";
+
+    }
+
 }
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
+$result = mysqli_query($db, "SELECT * FROM users");
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
-}
 ?>
