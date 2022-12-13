e5b1fa800694d9a5e610c3f608ec54fb233d5565
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-13 00:28:55 -0700

    update

--- single_post.php
+++ single_post.php
@@ -275,7 +275,7 @@ if (mysqli_num_rows($result) > 0) {
 
     <nav class="mt-4" aria-label="breadcrumb">
       <ol class="breadcrumb">
-        <li class="breadcrumb-item" style="font-size: 12px;"><img src="<?php echo $profile_picture; ?>" width="150px" height="150px" alt=""></li>&nbsp;&nbsp;
+        <li class="breadcrumb-item" style="font-size: 12px;"><?php echo $profile_picture; ?><img src="<?php echo $profile_picture; ?>" width="150px" height="150px" alt=""></li>&nbsp;&nbsp;
         <?php }} ?>
         <li class="breadcrumb-item" style="font-size: 12px;"><a href="#" class="text-decoration-none text-uppercase text-white"><?php echo $row['author']; ?></a></li>
         <li class="breadcrumb-item" style="font-size: 12px;"><a href="#" class="text-decoration-none text-uppercase text-white"><?php echo $row['published_at']; ?></a></li>
