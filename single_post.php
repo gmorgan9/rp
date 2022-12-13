5a6253939981ee5908d900069682d6fc9cb17e77
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-13 16:28:23 -0700

    update

--- single_post.php
+++ single_post.php
@@ -110,7 +110,7 @@ if(isset($_POST['register'])){
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <style>
-      code {
+      pre {
         background-color: pink;
       }
     </style>
