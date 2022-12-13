8378b7e07d28f2c20fb88383e2a6d2b26b6acbbb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-13 00:14:39 -0700

    update

--- pages/profile.php
+++ pages/profile.php
@@ -97,7 +97,7 @@ if (mysqli_num_rows($result) > 0) {
                     <div class="btn btn-outline-secondary">
 
                     <?php
-                    $sql="SELECT count('1') FROM posts WHERE author_idno = '".$_SESSION['user_idno']."'";
+                    $sql="SELECT count('1') FROM posts WHERE author_idno = '".$_SESSION['user_idno']."' AND status = 'published'";
                     $result=mysqli_query($conn,$sql);
                     $rowtotal=mysqli_fetch_array($result); 
                     echo "$rowtotal[0] Blog posts";
