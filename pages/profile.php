704a2f08617b2ef86c0c4a1291dc604ee430db1f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-13 00:21:38 -0700

    update

--- pages/profile.php
+++ pages/profile.php
@@ -95,7 +95,7 @@ if (mysqli_num_rows($result) > 0) {
                   <div class="justify-content-center mb-2">
                     <h6>Blog Post Stats</h6>
                     <div class="d-flex justify-content-center">
-                        <div class="badge text-bg-secondary me-2">
+                        <div class="badge text-bg-success me-2">
                         <?php
                         $sql="SELECT count('1') FROM posts WHERE author_idno = '".$_SESSION['user_idno']."' AND status = 'published'";
                         $result=mysqli_query($conn,$sql);
@@ -103,7 +103,7 @@ if (mysqli_num_rows($result) > 0) {
                         echo "$rowtotal[0] Published";
                         ?>
                         </div>
-                        <div class="badge text-bg-secondary">
+                        <div class="badge text-bg-primary">
                         <?php
                         $sql="SELECT count('1') FROM posts WHERE author_idno = '".$_SESSION['user_idno']."' AND status = 'draft'";
                         $result=mysqli_query($conn,$sql);
