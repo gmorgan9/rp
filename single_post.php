de636fb14cb6e07bb117b9cb4c8efbbea20b9f1a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-13 16:50:19 -0700

    update

--- single_post.php
+++ single_post.php
@@ -311,7 +311,9 @@ $comma = mysqli_query($conn, $grab);
 while($rows = mysqli_fetch_assoc($comma)) {
    $mark = explode(',', $rows['tags']);//what will do here
    foreach($mark as $out) {
+      echo "<div class='btn btn-outline-secondary'>"; 
       echo $out;
+      echo "</div>";
    }
 }
 
