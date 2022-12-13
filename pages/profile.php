1fd70fdb2e574d9137446be041de3cab95daebe5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-12 17:19:45 -0700

    update

--- pages/profile.php
+++ pages/profile.php
@@ -39,7 +39,7 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    echo "File is an image - .";
     $uploadOk = 1;
   } else {
     echo "File is not an image.";
