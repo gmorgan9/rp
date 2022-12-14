7e37df3af49669d802f7747bd33b778e3c14baa0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-13 17:42:27 -0700

    update

--- single_post.php
+++ single_post.php
@@ -351,7 +351,7 @@ if (mysqli_num_rows($another) > 0) {
   <h4 class="text-center"><?php echo $author; ?></h4>
   <a
   class=""
-  style="background-color: #ac2bac; color: white; padding: 10px; border-radius: 50px;"
+  style="background-color: #ac2bac; color: white; padding: 5px 10px; border-radius: 50px;"
   href="#!"
   role="button"
   ><i class="bi bi-instagram"></i
