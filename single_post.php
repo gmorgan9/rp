2a049778f56bae496ccbc7a8cdf12156d2bf8e5d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-13 19:50:18 -0700

    update

--- single_post.php
+++ single_post.php
@@ -460,7 +460,7 @@ if (mysqli_num_rows($another) > 0) {
 </div>
 </div>
 
-<button onclick="topFunction()" id="myBtn" title="Go to top"><strong><i class="bi bi-bold bi-arrow-up"></i></strong></button>
+<button onclick="topFunction()" id="myBtn" title="Go to top"><i style="font-weight: bold;" class="bi bi-arrow-up"></i></button>
 
 
 <script>
