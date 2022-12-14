dec77c1eee3cdd56f97371b2f0f1320c6ca7ffc8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-13 17:45:21 -0700

    update

--- single_post.php
+++ single_post.php
@@ -351,7 +351,7 @@ if (mysqli_num_rows($another) > 0) {
   <h4 class="text-center"><?php echo $author; ?></h4>
   <a
   class=""
-  style="background-color: #ac2bac; color: white; padding: 7px 10px; border-radius: 75px;"
+  style="background-color: #03c6fc; color: white; padding: 7px 10px; border-radius: 75px;"
   href="https://resume.morganserver.com/"
   target="_blank"
   ><i class="bi bi-globe"></i
