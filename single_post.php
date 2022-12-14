30f1e1dcd3ceaa398c3d5aaa5d2155cdecfded97
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-13 17:02:46 -0700

    update

--- single_post.php
+++ single_post.php
@@ -297,7 +297,7 @@ if (mysqli_num_rows($another) > 0) {
 
     <br><br>
 <div class="top-hr d-flex">
-&nbsp;&nbsp;<hr style="width:35%;"> &nbsp;&nbsp; <img style="border: 1px solid #969696; border-radius: 100%; margin-top: -10px;" src="<?php echo $profile_picture; ?>" width="60px" height="60px" alt=""> &nbsp;&nbsp; <hr style="width:35%;">&nbsp;&nbsp;
+&nbsp;&nbsp;<hr style="width:45%;"> &nbsp;&nbsp; <img style="border: 1px solid #969696; border-radius: 100%; margin-top: -10px;" src="<?php echo $profile_picture; ?>" width="60px" height="60px" alt=""> &nbsp;&nbsp; <hr style="width:45%;">&nbsp;&nbsp;
 </div>
 
 <br><br>
