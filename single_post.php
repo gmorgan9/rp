12ca63991dc578eec90b55e1ed49293916a9c890
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-13 17:08:32 -0700

    update

--- single_post.php
+++ single_post.php
@@ -345,7 +345,7 @@ if (mysqli_num_rows($another) > 0) {
   </div>
 
   <div class="top-hr d-flex">
-  &nbsp;&nbsp;<hr style="width:45%;"> &nbsp;&nbsp; <hr style="width:45%;">&nbsp;&nbsp;
+  &nbsp;&nbsp;<hr style="width:90%;">&nbsp;&nbsp;
   </div>
 
 
