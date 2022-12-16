a7d9a66921634b367dfd9c570a865d8ef3e38390
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-16 13:21:55 -0600

    update

--- single_post.php
+++ single_post.php
@@ -126,7 +126,7 @@ session_start();
         cursor: pointer;
         border-radius: 5px;
         height: 20px;
-        padding: 5px 0px;
+        padding: 0px 5px;
         font-size: 12px;
       }
       .website-btn:hover {
