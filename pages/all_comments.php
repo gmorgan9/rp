5eae930161b1485d4cfe73609e456a42c6efa07b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-16 13:57:51 -0600

    update

--- pages/all_comments.php
+++ pages/all_comments.php
@@ -12,32 +12,34 @@ session_start();
 ?>
 
 <?php
-if(isset($_POST['add'])){
-  $idno  = rand(10000, 99999); // figure how to not allow duplicates
-  $title = mysqli_real_escape_string($conn, $_POST['title']);
-  $content = mysqli_real_escape_string($conn, $_POST['content']);
-  $author = mysqli_real_escape_string($conn, $_POST['author']);
-  $category = mysqli_real_escape_string($conn, $_POST['category']);
-  $tags = mysqli_real_escape_string($conn, $_POST['tags']);
-  $status = mysqli_real_escape_string($conn, $_POST['status']);
-  $author_idno = mysqli_real_escape_string($conn, $_POST['author_idno']);
+
+date_default_timezone_set('America/Denver');
+$date = date('F d, Y', time());
 
 
-  $select = " SELECT * FROM posts WHERE title = '$title'";
+if (isset($_POST['published'])) {
+  $appUpdateQuery = "UPDATE posts SET status = 'published', published_at = '$date' WHERE post_id = '".$_POST['post_id']."'";
+  $appUpdateResult = mysqli_query($conn, $appUpdateQuery);
+  header('location: all_posts.php');
+}
+?>
 
-  $result = mysqli_query($conn, $select);
+<?php
+if (isset($_POST['draft'])) {
+  $appUpdateQuery = "UPDATE posts SET status = 'draft', published_at = null WHERE post_id = '".$_POST['post_id']."'";
+  $appUpdateResult = mysqli_query($conn, $appUpdateQuery);
+  header('location: all_posts.php');
+}
+?>
 
-  if(mysqli_num_rows($result) > 0){
-
-     $error[] = 'title already exist!';
-
-  }else {
-        $insert = "INSERT INTO posts (idno, title, content, author, author_idno, category, tags) VALUES ('$idno', '$title','$content','$author','$author_idno','$category', '$tags')";
-        mysqli_query($conn, $insert);
-        header('location: all_posts.php');
-     }
-
-};
+<?php 
+// START DELETE
+  if (isset($_POST['delete'])) {
+    $delete = "DELETE FROM posts WHERE post_id = '".$_POST['post_id']."'";
+    $terUpdateResult = mysqli_query($conn, $delete);
+    header('location: all_posts.php');
+  }
+// END DELETE
 
 ?>
 
@@ -55,34 +57,13 @@ if(isset($_POST['add'])){
 
     <link rel="stylesheet" href="../assets/styles.css?v=2.50">
 
-    <script src="https://cdn.tiny.cloud/1/7kainuaawjddfzf3pj7t2fm3qdjgq5smjfjtsw3l4kqfd1h4/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-
-    <title>Add New Post - CacheUp Blog</title>
+    <title>All Posts - CacheUp Blog</title>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-
-<?php
-$user_id = $_SESSION['user_id'];
-$select = "SELECT * FROM users WHERE user_id = $user_id";
-$result = mysqli_query($conn, $select);
-if (mysqli_num_rows($result) > 0) {
-   while($row = mysqli_fetch_assoc($result)) {
-    $firstname    = $row['firstname'];
-    $lastname     = $row['lastname'];
-    $idno         = $row['idno'];
-}}
-$select = "SELECT * FROM categories";
-$result = mysqli_query($conn, $select);
-if (mysqli_num_rows($result) > 0) {
-   while($row = mysqli_fetch_assoc($result)) {
-    $cat_id    = $row['cat_id'];
-    $category  = $row['category'];
-}}
-?>
     
 <div class="main-container">
 <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
@@ -91,114 +72,79 @@ if (mysqli_num_rows($result) > 0) {
         
 <div class="main">
     <div class="page-header mx-auto">
-        <p class="page_title">Add New Post</p>
+        <p class="page_title">All Posts</p>
     </div>
 
     <div class="main-content">
-    
 
+    <table class="table table-dark" style="width: 99%;">
+  <thead>
+    <tr>
+      <th style="background-color: #1a1a1a;" cope="col">ID #</th>
+      <th style="background-color: #1a1a1a;" scope="col">Title</th>
+      <th style="background-color: #1a1a1a;" scope="col">Status</th>
+      <th style="background-color: #1a1a1a;" scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
 
-    <form action="" method="POST">
-      <input class="form-control" style="width: 99%;" type="hidden" name="author_idno" value="<?php echo $idno;?>">
-        <h3>
-            New Post
-        </h3>
-        <div class="modal_help float-end" style="margin-right: 25px; margin-top: -55px !important;">
-
-          <!-- Button trigger modal -->
-            <button type="button" style="background: none; color: inherit; border: none; cursor: pointer; outline: inherit;" class="badge text-bg-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-              Instructions
-            </button>
-
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog modal-dialog-centered modal-lg">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5 text-black" id="exampleModalLabel">Instructions</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body text-black">
-                    Things you will want to pay attention to while creating a new post for our blog. If you have any questions, please reach out via email, I will try and get back to you all as soon as possible.
-                    <ul>
-                      <li>For all images wanting to be insertted, please have a link for your image ready. a useful site to help you get a link for images would be: <a href="https://postimages.org" target="_blank">https://postimages.org</a>.</li>
-                        <li>Sizes for images to fit inside of blog block.</li>
-                    </ul>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary text-white" data-bs-dismiss="modal">Close</button>
-                    <!-- <button type="button" class="btn btn-primary text-black">Save changes</button> -->
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
+  <?php
+      $sql = "SELECT * FROM posts";
+      $all = mysqli_query($conn, $sql);
+      if($all) {
+          while ($row = mysqli_fetch_assoc($all)) {
+            $post_id     = $row['post_id'];
+            $idno      = $row['idno'];
+            $title     = $row['title'];
+            $status = $row['status'];
+            ?>
+    <tr>
+        <?php //if($_SESSION['empID'] != $row['employeeID']){ ?>
+        <th style="background-color: #1a1a1a;" scope="row"><?php echo $idno; ?></th>
+        <td style="background-color: #1a1a1a;"><?php echo $title; ?></td>
+        <?php if($status == 'draft') { ?>
+          <td style="background-color: #1a1a1a;">
+        <form method="post" action="">
+          <input type="hidden" name="post_id" value="<?php echo $post_id; ?>" />
+          <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="published"><span class="badge text-bg-success">Publish</span></button>
+        </form>
+        </td>
+        <?php } else { ?>
+          <td style="background-color: #1a1a1a;">
+        <form method="post" action="">
+          <input type="hidden" name="post_id" value="<?php echo $post_id; ?>" />
+          <button style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="draft"><span class="badge text-bg-primary">Draft</span></button>
+        </form>
+        <?php } ?>
+        </td>
+        <td style="background-color: #1a1a1a;">
+          <div class="d-flex">
+            <a style="text-decoration: none; background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" href="actions/edit_post.php?id=<?php echo $post_id; ?>"><span class="badge text-bg-success">View</span></a>
+            &nbsp;
+            <form method="post" action="">
+              <input type="hidden" name="post_id" value="<?php echo $post_id; ?>" />
+              <button onclick="return confirm('Be Careful, Can\'t be undone! \r\nOK to delete?')" style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="delete"><span class="badge text-bg-danger">Delete</span></button>
+            </form>
+          </div>
+        </td>
+        <?php }}?>
+  </tbody>
+</table>
 
 
         </div>
-        <div>
-            <label>Post Title</label>
-            <input class="form-control" style="width: 99%;" type="text" name="title">
-        </div>
-        <div class="pt-3"></div>
-        <div>
-            <label>Category</label>
-            <select style="width: 99%;" name="category" class="form-control">
-                <option value="">Select one...</option>
-                <option value="none">None</option>
-                <?php
-                $query ="SELECT * FROM categories";
-                $result = $conn->query($query);
-                if($result->num_rows> 0){
-                  $options= mysqli_fetch_all($result, MYSQLI_ASSOC);
-                }
-                ?>
-                <?php 
-                    foreach ($options as $option) {
-                ?>
-                <option value="<?php echo $option['category']; ?>"><?php echo $option['category']; ?></option>
-                <?php } ?>
-            </select>
-        </div>
-        <div class="pt-3"></div>
-        <div>
-            <label>Tags</label>
-            <input class="form-control" style="width: 99%;" type="text" name="tags">
-        </div>
-        <br>
-        <textarea name="content" id="content" style="width: 99%;"></textarea>
-        <input type="hidden" name="author" value="<?php echo $firstname; ?>&nbsp;<?php echo $lastname; ?>">
-        <br>
-        <input type="submit" name="add" value="Submit" class="btn btn-light btn-block"> &nbsp;
-        <button class="btn btn-dark btn-block" onclick="window.history.go(-1); return false;">Go Back</button>
-    </form>
-   
-    </div>
-    
+
+
+
+
     
 </div>
 
+    
 
 
 
 
-
-
-
-<script>
-    tinymce.init({
-      selector: 'textarea#content',
-      plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect',
-      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-      tinycomments_mode: 'embedded',
-      tinycomments_author: 'Author name',
-      mergetags_list: [
-        { value: 'First.Name', title: 'First Name' },
-        { value: 'Email', title: 'Email' },
-      ]
-    });
-  </script>
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script type="text/javascript">
   $(document).on('click', 'a', function() {
@@ -207,7 +153,12 @@ if (mysqli_num_rows($result) > 0) {
   $(document).on('click', 'ul li a', function() {
     $(this).removeClass('active')
   })
+  $(document).on('click', 'td a', function() {
+    $(this).removeClass('active')
+  })
 </script>
+
+
     <script src="../assets/js/dropdown.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
