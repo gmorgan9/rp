6fe64d7cc783a8b1801e8a8fb985c57a463bd72b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-16 13:16:33 -0600

    update

--- single_post.php
+++ single_post.php
@@ -497,7 +497,7 @@ if (mysqli_num_rows($result) > 0) {
         <p class="text-muted"><?php echo $option['content']; ?></p>
         <div class="right-content d-flex justify-content-end" style="margin-top: -70px;">
           <p class="text-muted" style="font-size: 12px;"><?php echo date('F j, Y / g:i a', strtotime($option['submit_date'])); ?></p>
-          <span class="badge bg-secondary"><a href="#">Reply</a></span>
+          <span class="badge"><a href="#">Reply</a></span>
         </div>
       </div>
       <br>
