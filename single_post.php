5457aa6b8d679483391fc58a93ff38cb5b36f676
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-16 13:36:42 -0600

    update

--- single_post.php
+++ single_post.php
@@ -513,14 +513,14 @@ if (mysqli_num_rows($result) > 0) {
         <h5><?php echo $option['name']; ?></h5>
         <div class="mt-3"></div>
         <p class="text-muted"><?php echo $option['content']; ?></p>
-        <div class="right-content d-flex justify-content-end" style="margin-top: -95px;">
+        <div class="right-content d-flex justify-content-end" style="">
           <p class="text-muted" style="font-size: 12px;"><?php echo date('F j, Y / g:i a', strtotime($option['submit_date'])); ?></p>
           &nbsp;&nbsp;
           <a class="reply text-muted" href="#">Reply</a>
         </div>
       </div>
       <br>
-      <hr style="margin-top: 55px;">
+      <hr style="">
 
 
     <?php } ?>
