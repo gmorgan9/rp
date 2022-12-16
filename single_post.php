30e94e44f87adf3c959f3edfac90260b68969309
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-16 14:14:16 -0600

    update

--- single_post.php
+++ single_post.php
@@ -454,7 +454,7 @@ if (mysqli_num_rows($result) > 0) {
     <form action="" method="POST">
     <h4>Leave a Comment</h4>
     <input type="hidden" name="post_idno" value="<?php echo $post_idno; ?>" class="text-muted form-control">
-    <input type="text" name="post_id" value="<?php echo $id; ?>" class="text-muted form-control">
+    <input type="hidden" name="post_id" value="<?php echo $id; ?>" class="text-muted form-control">
     <p class="text-muted" style="font-size: 12px;">Your email address will not be published. Required fields are marked <span style="color: red;">*</span></p>
     <div class="d-flex">
       <div class="form-group w-50">
