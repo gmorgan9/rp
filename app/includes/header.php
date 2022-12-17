34c96fe443f22fc98a37e452df8303dd1522c09c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 13:50:01 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,4 +1,4 @@
 <div class="page-header d-flex">
-    <a style="text-decoration: none;" href="/"><i class="bi bi-house"></i>CacheUp</a>
+    <a style="text-decoration: none; color: white" href="/"><i style="font-size: 12px;" class="bi bi-house"></i> &nbsp; CacheUp</a>
     <p class="page_title">Dashboard</p>
 </div>
