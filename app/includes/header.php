17dab4a56ccaec4e426b18ffe51a33e9e790430e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 14:14:19 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -3,5 +3,6 @@
     <a style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door"></i>&nbsp;CacheUp</a>
     &nbsp;&nbsp;&nbsp;
     <a style="text-decoration: none; color: white" href="/"><i class="bi bi-chat-right"></i>&nbsp;0</a>
+    &nbsp;&nbsp;&nbsp;
     <a style="text-decoration: none; color: white" href="/"><i class="bi bi-plus"></i>&nbsp;New</a>
 </div>
