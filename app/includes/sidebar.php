7d82ee8f6482177247545d9e463660ddc8b5680b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 15:28:25 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -58,7 +58,7 @@ if (mysqli_num_rows($result) > 0) {
 
                 <div class="d-flex">
                     <div class="dropdown me-1">
-                        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="25,-10">
+                        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="150,-50">
                             Offset
                         </button>
                         <ul class="dropdown-menu">
