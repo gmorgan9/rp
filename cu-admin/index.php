4e2e5f902f50d372b5a5b5df3d6d86d17012de75
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 14:34:10 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -76,8 +76,8 @@ if(isLoggedIn() == false){
 }
 
 .side:hover {
-  border-left: 4px solid red;
-  color: red !important;
+  border-left: 4px solid #325A73;
+  color: #325A73 !important;
 }
     </style>
     
