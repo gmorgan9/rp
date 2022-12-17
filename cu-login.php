ff4c60faa0361b697a11447bd9eead1c3e3f533c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 10:55:30 -0600

    update

--- cu-login.php
+++ cu-login.php
@@ -7,6 +7,10 @@
     <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
     <title>Log In &lsaquo; CacheUp</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
     <!-- custom styles -->
         <style>
             body {
