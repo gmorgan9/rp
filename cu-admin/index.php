9b73099bdd25d0de52cdfd0ebe1c144728959885
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 13:38:56 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -23,7 +23,7 @@ if(isLoggedIn() == false){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 
-    <link rel="stylesheet" href="../assets/styles.css?v=3.23">
+    <link rel="stylesheet" href="../assets/styles.css?v=3.24">
 
     <title>Dashboard - CacheUp Blog</title>
 
