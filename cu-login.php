58570740b0e49473359a2d7aa8667a313496fd6a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 10:53:44 -0600

    update

--- cu-login.php
+++ cu-login.php
@@ -18,7 +18,7 @@
 </head>
 <body>
 
-    <div class="login-form">
+    <div class="login-form mx-auto">
         <h1 class="logo">
             <img src="assets/images/fav.png" width="84px" height="84px" alt="">
         </h1>
