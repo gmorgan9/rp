161a7e90e72f1c4ac7abcf899ff2700849aa49b9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 14:49:30 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,13 +1,16 @@
-<div class="page-header d-flex">
-    <div class="text-start">
-        &nbsp;&nbsp;&nbsp;<img src="../assets/images/new-logo.png" height="20px" width="20px" alt="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <a style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door-fill"></i>&nbsp; CacheUp</a>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <a style="text-decoration: none; color: white" href="/"><i class="bi bi-chat-right"></i>&nbsp;0</a>
-        &nbsp;
-        <a style="text-decoration: none; color: white" href="/"><i class="bi bi-plus"></i>New</a>
-    </div>
-    <div class="text-end">
-        text
-    </div>
+<div class="page-header d-flex text-start">
+    &nbsp;&nbsp;&nbsp;<img src="../assets/images/new-logo.png" height="20px" width="20px" alt="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door-fill"></i>&nbsp; CacheUp</a>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a style="text-decoration: none; color: white" href="/"><i class="bi bi-chat-right"></i>&nbsp;0</a>
+    &nbsp;
+    <a style="text-decoration: none; color: white" href="/"><i class="bi bi-plus"></i>New</a>
+</div>
+<div class="page-header d-flex text-end">
+    &nbsp;&nbsp;&nbsp;<img src="../assets/images/new-logo.png" height="20px" width="20px" alt="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door-fill"></i>&nbsp; CacheUp</a>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a style="text-decoration: none; color: white" href="/"><i class="bi bi-chat-right"></i>&nbsp;0</a>
+    &nbsp;
+    <a style="text-decoration: none; color: white" href="/"><i class="bi bi-plus"></i>New</a>
 </div>
