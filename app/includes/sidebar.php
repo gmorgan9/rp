9b0e9e9cd6a107fed576178f9ecd80ab73dcf6cd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 14:40:57 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -29,6 +29,7 @@ if (mysqli_num_rows($result) > 0) {
             <!-- END DASHBOARD -->
 
             <!-- POSTS -->
+            <div class="pt-2"></div>
             <br>
             <a href="#" s style="text-decoration: none;" class="text-muted ps-2 side" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                 <i class="bi bi-pin-angle"></i>&nbsp;
