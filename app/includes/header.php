9006297de76e8ccbfbfd3e6ce2b7fe762bb032f9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 14:14:03 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -2,11 +2,6 @@
     &nbsp;<img src="../assets/images/new-logo.png" height="20px" width="20px" alt="">&nbsp;&nbsp;
     <a style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door"></i>&nbsp;CacheUp</a>
     &nbsp;&nbsp;&nbsp;
-    <div class="dropdown">
-        <span>Mouse over me</span>
-        <div class="dropdown-content">
-            <p>Hello World!</p>
-        </div>
-    </div>
+    <a style="text-decoration: none; color: white" href="/"><i class="bi bi-chat-right"></i>&nbsp;0</a>
     <a style="text-decoration: none; color: white" href="/"><i class="bi bi-plus"></i>&nbsp;New</a>
 </div>
