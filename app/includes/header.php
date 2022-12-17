f1ca3ea5158e0f982067031b92fa72e3388c1646
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 14:09:07 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,6 +1,6 @@
 <div class="page-header d-flex">
-    <img src="../assets/images/new-logo.png" height="20px" width="20px" alt="">&nbsp;&nbsp;
+    &nbsp;<img src="../assets/images/new-logo.png" height="20px" width="20px" alt="">&nbsp;&nbsp;
     <a style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door"></i>&nbsp;CacheUp</a>
     &nbsp;&nbsp;&nbsp;
-    <p class="page_title">Dashboard</p>
+    <a style="text-decoration: none; color: white" href="/"><i class="bi bi-plus"></i>&nbsp; New</a>
 </div>
--- cu-admin/index.php
+++ cu-admin/index.php
@@ -89,6 +89,16 @@ if(isLoggedIn() == false){
   })
 </script>
 
+<script>
+  $(document).ready(function(){
+    $('ul.nav li.dropdown').hover(function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+    }, function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+    });  
+});
+</script>
+
 
     <script src="../assets/js/dropdown.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
