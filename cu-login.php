06686bf4c2a54107fc09963b1c34ecda43128ca8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 11:20:57 -0600

    update

--- cu-login.php
+++ cu-login.php
@@ -50,7 +50,7 @@
                 <input type="password" id="user_pass" name="password" class="form-control" autocapitalize="off">
             </div>
             <br>
-            <input type="submit" name="login" class="btn btn-primary align-end" value="Log In">
+            <input type="submit" name="login" class="btn btn-primary float-end" value="Log In">
         </form>
     </div>
     
