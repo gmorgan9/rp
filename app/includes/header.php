c51e2de19faede9fbcbbee11053926565d06514f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 14:59:41 -0600

    upda

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,9 +1,9 @@
 <div class="page-header d-flex text-start">
     <div class="pe-2"></div>
     <img src="../assets/images/new-logo.png" height="20px" width="20px" alt="">
-    <div class="pe-2"></div>
+    <div class="pe-4"></div>
     <a style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door-fill"></i>&nbsp; CacheUp</a>
-    <div class="pe-2"></div>
+    <div class="pe-3"></div>
     <a style="text-decoration: none; color: white" href="/"><i class="bi bi-chat-right"></i>&nbsp;0</a>
     <div class="pe-2"></div>
     <a style="text-decoration: none; color: white" href="/"><i class="bi bi-plus"></i>New</a>
