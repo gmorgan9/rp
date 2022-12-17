6a04b346f17173e6312be0fca551eebeff24f12f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 10:58:55 -0600

    update

--- cu-login.php
+++ cu-login.php
@@ -27,8 +27,14 @@
             <img src="assets/images/fav.png" width="84px" height="84px" alt="">
         </h1>
         <form action="" method="POST">
-
-
+            <div class="username">
+                <label for="user_login">Username</label>
+                <input type="text" id="user_login" name="username" class="form-control" autocapitalize="off">
+            </div>
+            <div class="password">
+                <label for="user_pass">Username</label>
+                <input type="password" id="user_pass" name="password" class="form-control" autocapitalize="off">
+            </div>
         </form>
     </div>
     
