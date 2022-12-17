7ecdb1b1e8b1120007ace98661c795d03c6367cf
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 13:49:00 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,4 +1,4 @@
 <div class="page-header">
     <a href="/"><i class="bi bi-house"></i>CacheUp</a>
-    <p class="page_title" style="float: left; padding-top: 2px;">Dashboard</p>
+    <p class="page_title">Dashboard</p>
 </div>
