0372f764ec719c8a7a2f176579500fa3b49fecfb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 15:11:26 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,13 +1,13 @@
 <div class="page-header">
-    <div class="d-inline" style="float:left !important; display: block;">
+    <div style="float:left !important; display: block;">
         <div class="pe-2"></div>
-        <img src="../assets/images/new-logo.png" height="20px" width="20px" alt="">
+        <img class="d-inline" src="../assets/images/new-logo.png" height="20px" width="20px" alt="">
         <div class="pe-4"></div>
-        <a style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door-fill"></i>&nbsp;CacheUp</a>
+        <a class="d-inline" style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door-fill"></i>&nbsp;CacheUp</a>
         <div class="pe-4"></div>
-        <a style="text-decoration: none; color: white" href="/"><i class="bi bi-chat-right"></i>&nbsp;0</a>
+        <a class="d-inline" style="text-decoration: none; color: white" href="/"><i class="bi bi-chat-right"></i>&nbsp;0</a>
         <div class="pe-2"></div>
-        <a style="text-decoration: none; color: white" href="/"><i class="bi bi-plus"></i>New</a>
+        <a class="d-inline" style="text-decoration: none; color: white" href="/"><i class="bi bi-plus"></i>New</a>
     </div>
     <div style="float: right !important;">
         <a style="text-decoration: none; color: white" href="/"><i class="bi bi-plus"></i>New</a>
