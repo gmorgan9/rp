7bd373578fc50e49791cde73ef276cd23ba45839
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 14:27:22 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -23,7 +23,7 @@ if (mysqli_num_rows($result) > 0) {
             <!-- DASHBOARD -->
             <span>
             <a href="<?php echo BASE_URL . '/cu-admin/' ?>" style="text-decoration: none;" class="ps-2 text-muted side" aria-current="true">
-                &nbsp;<i class="bi bi-speedometer2"></i>
+                <i class="bi bi-speedometer2"></i>&nbsp;
                 <span>  Dashboard</span>
             </a>
             </span>
