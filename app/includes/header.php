293096ed8314f3ae8cd9ec3ddf4bbb2321a7c2cc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 15:12:01 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -3,7 +3,7 @@
         <div class="pe-2"></div>
         <img class="d-inline" src="../assets/images/new-logo.png" height="20px" width="20px" alt="">
         <div class="pe-4"></div>
-        <a class="d-inline" style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door-fill"></i>&nbsp;CacheUp</a>
+        <div class="d-inline"><a style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door-fill"></i>&nbsp;CacheUp</a></div>
         <div class="pe-4"></div>
         <a class="d-inline" style="text-decoration: none; color: white" href="/"><i class="bi bi-chat-right"></i>&nbsp;0</a>
         <div class="pe-2"></div>
