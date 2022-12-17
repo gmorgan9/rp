de80820773af7f5f50dbfd94a461b7857aaef5dd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-17 14:04:51 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,5 +1,5 @@
 <div class="page-header d-flex">
-    <img src="../assets/images/new-logo.png" height="25px" width="25px" alt="">
+    <img src="../assets/images/new-logo.png" height="25px" width="25px" alt="">&nbsp;&nbsp;
     <a style="text-decoration: none; color: white" href="/"><i class="bi bi-house-door"></i>&nbsp;CacheUp</a>
     &nbsp;&nbsp;&nbsp;
     <p class="page_title">Dashboard</p>
