9e84e8fda7ca6d328f978093f1fafd2245380959
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-18 08:34:09 -0600

    update

--- cu-admin/profile.php
+++ cu-admin/profile.php
@@ -69,7 +69,7 @@ if (mysqli_num_rows($result) > 0) {
 <div class="mt-5"></div>
   <div class="row">
     <div class="col-2"></div>
-<div class="col-10" style="margin-left: -30px;">
+<div class="col-10" style="margin-left: -40px;">
     <section>
           <div class="row">
             <div class="col-lg-4">
