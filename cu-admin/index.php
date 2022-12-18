da1597a63525c1918b0ecb18b67564a5a63c936a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-18 07:50:13 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -116,64 +116,29 @@ if(isLoggedIn() == false){
 <!-- main-container -->
   <div class="container-fluid" style="margin:0;padding:0;">
 
-  <div class="row">
-    <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
-  </div>
-  <div class="row">
-    <div class="col">
-      <?php include(ROOT_PATH . "/app/includes/sidebar.php") ?>
-    </div>
-  </div>
-
-
-
-
-    <div class="main">
-
+    <div class="row">
+      <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
     </div>
 
+    <div class="row">
+      <div class="col">
+        <?php include(ROOT_PATH . "/app/includes/sidebar.php") ?>
+      </div>
+    </div>
+
+    <div class="row float-bottom">
+      <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+    </div>
 
   </div>
 <!-- END main-container -->
 
 
-<?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+  
+  
 
-<!-- <script src="../assets/js/new.js"></script> -->
-
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script type="text/javascript">
-  // $(document).on('click', 'a', function() {
-  //   $(this).addClass('active').siblings().removeClass('active')
-  // })
-  // $(document).on('click', 'ul li a', function() {
-  //   $(this).removeClass('active')
-  // })
-</script>
-
-<script>
-// $(document).ready(function () {
-//     $('.dropdown-toggle').mouseover(function() {
-//         $('.dropdown-menu').show();
-//     })
-
-//     $('.dropdown-toggle').mouseout(function() {
-//         t = setTimeout(function() {
-//             $('.dropdown-menu').hide();
-//         }, 100);
-
-//         $('.dropdown-menu').on('mouseenter', function() {
-//             $('.dropdown-menu').show();
-//             clearTimeout(t);
-//         }).on('mouseleave', function() {
-//             $('.dropdown-menu').hide();
-//         })
-//     })
-// })
-</script>
-
-
-    <script src="../assets/js/dropdown.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+  <script src="../assets/js/dropdown.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
