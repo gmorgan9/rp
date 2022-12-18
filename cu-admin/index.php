90434cef662225dd1856d6a93d35697331a9a207
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-18 08:43:08 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -67,6 +67,7 @@ if(isLoggedIn() == false){
 
   <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
   <script src="../assets/js/dropdown.js"></script>
+  <script src="../assets/js/bar.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
