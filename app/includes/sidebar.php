a4575ac050591e09480957d5d359f613c3910f08
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-18 07:53:52 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -12,7 +12,7 @@ if (mysqli_num_rows($result) > 0) {
 
 <div id="sidebarMenu" class=" sidebar">
     
-<div class="position-sticky">
+<div class="position-fixed">
         
         <!-- </div> -->
         <div class="list-group list-group-flush mt-4">
