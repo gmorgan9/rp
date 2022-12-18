ca4fb1d3a4d306b69c5a93e113a9b5b0a62d2402
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-18 07:35:51 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -72,7 +72,7 @@ if(isLoggedIn() == false){
         display: inline-block;
       }
       .menu-content {
-        right: 25 !important;
+        right: 150 !important;
         padding-top: 10px;
         margin-top: 1px;
         background-color: #2d3337;
