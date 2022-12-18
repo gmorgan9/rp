b458a72f4f44f5d9db9fa37ee4d34125ed72050a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-18 08:31:15 -0600

    update

--- cu-admin/profile.php
+++ cu-admin/profile.php
@@ -52,20 +52,23 @@ if (mysqli_num_rows($result) > 0) {
     $profile_picture  = $row['profile_picture'];
 }}
 ?>
-    
-<div class="main-container">
-<?php include(ROOT_PATH . "/app/includes/header.php"); ?>
 
-<?php include(ROOT_PATH . "/app/includes/sidebar.php") ?>
-        
-<div class="main">
-    <div class="page-header mx-auto">
-        <p class="page_title">Profile</p>
-    </div>
+<!-- main-container -->
+<div class="container-fluid">
 
-    <div class="main-content">
+<div class="row">
+  <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+</div>
 
-    <section>
+<div class="row">
+  <div class="col" style="margin:0;padding:0;">
+    <?php include(ROOT_PATH . "/app/includes/sidebar.php") ?>
+  </div>
+</div>
+
+
+
+<section>
           <div class="row">
             <div class="col-lg-4">
               <div class="card mb-4" style="background-color: #1f1f1f;">
@@ -162,10 +165,21 @@ if (mysqli_num_rows($result) > 0) {
 
 
 
-    </div>  
 
-    
+
+
+
+
+<div class="row">
+  <div class="col-2"></div>
+  <div class="col position-absolute bottom-0 ms-5">
+    <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+  </div>
 </div>
+
+</div>
+<!-- END main-container -->
+
 
 
 
@@ -176,15 +190,7 @@ if (mysqli_num_rows($result) > 0) {
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script type="text/javascript">
-  $(document).on('click', 'a', function() {
-    $(this).addClass('active').siblings().removeClass('active')
-  })
-  $(document).on('click', 'ul li a', function() {
-    $(this).removeClass('active')
-  })
-</script>
-    <script src="../assets/js/dropdown.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script src="../assets/js/dropdown.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
