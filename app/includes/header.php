f646ebfb692fae7a766e5b60a839c475eb83b396
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-18 07:45:01 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -16,7 +16,7 @@
             </a>
             <div class="menu-content">
                 <div class="float-start">
-                    <i class="bi bi-person-square text-muted" style="font-size: 55px;margin-left: 20px; margin-top: -10%;"></i>
+                    <i class="bi bi-person-square text-muted" style="font-size: 60px;margin-left: 20px; margin-top: -15%;"></i>
                 </div>
                 <div class="float-end" style="margin-right: 15px;">
                     <a class="links text-white" href="<?php echo BASE_URL . '/cu-admin/profile.php' ?>"><?php echo $_SESSION['username']; ?></a>
