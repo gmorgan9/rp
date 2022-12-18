a4203e446408352b0e180a1851f42bf4a6aceccd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-18 09:06:34 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -12,34 +12,33 @@ if (mysqli_num_rows($result) > 0) {
 
 <!-- <div id="sidebarMenu" class="position-fixed sidebar"> -->
 <!-- close -->
-    <div>
+    <!-- <div> -->
         
         <!-- </div> -->
-        <div class="list-group list-group-flush mt-4">
+        <!-- <div class="list-group list-group-flush mt-4">
 
-			<div class="accordion mb-2" id="accordionExample">
+			<div class="accordion mb-2" id="accordionExample"> -->
 
             <!-- DASHBOARD -->
-                <a href="<?php echo BASE_URL . '/cu-admin/' ?>" style="text-decoration: none;" class="text-white ps-2 side">
+                <!-- <a href="<?php echo BASE_URL . '/cu-admin/' ?>" style="text-decoration: none;" class="text-white ps-2 side">
                     <i class="bi bi-speedometer2"></i>&nbsp;
                     <span>  Dashboard</span>
-                </a>
+                </a> -->
             <!-- END DASHBOARD -->
 
             <!-- POSTS -->
-                <div class="pt-2"></div>
+                <!-- <div class="pt-2"></div>
                 <div class="dropdown-menu">
                     <a style="text-decoration: none;" href="#" class="a-link ps-2 side menu-btn text-white">
                         <i class="bi bi-pin-angle"></i>&nbsp;
                         <span>  Posts</span>
                     </a>
                     <div class="menu-content" style="margin-left: 170px !important; margin-top: -30px;">
-                        <a class="links text-white" href="<?php echo BASE_URL . '/pages/all_posts.php' ?>">All Posts</a>
-                        <a class="links text-white" href="<?php echo BASE_URL . '/pages/add_posts.php' ?>">Add New</a>
-                        <a class="links text-white" href="<?php echo BASE_URL . '/pages/categories.php' ?>">Add New</a>
-                        <!-- <div class="pb-3"></div> -->
+                        <a class="links text-white" href="<?php //echo BASE_URL . '/pages/all_posts.php' ?>">All Posts</a>
+                        <a class="links text-white" href="<?php //echo BASE_URL . '/pages/add_posts.php' ?>">Add New</a>
+                        <a class="links text-white" href="<?php //echo BASE_URL . '/pages/categories.php' ?>">Add New</a>
                     </div>
-                </div>
+                </div> -->
 
 
 
@@ -48,42 +47,42 @@ if (mysqli_num_rows($result) > 0) {
 
             <!-- COMMENTS -->
                 <!-- <div class="pt-2"></div> -->
-                <a href="<?php echo BASE_URL . '/cu-admin/comments.php' ?>" style="text-decoration: none;" class="text-white ps-2 side">
+                <!-- <a href="<?php //echo BASE_URL . '/cu-admin/comments.php' ?>" style="text-decoration: none;" class="text-white ps-2 side">
                     <i class="bi bi-chat-right"></i>&nbsp;
                     <span>  Comments</span>
-                </a>
+                </a> -->
             <!-- END COMMENTS -->
 
             <!-- USERS -->
-				<div class="pt-2"></div>
+				<!-- <div class="pt-2"></div>
                 <a href="#<?php //echo BASE_URL . '/logout.php' ?>" style="text-decoration: none;" class="text-white ps-2 side">
                     <i class="bi bi-people"></i>&nbsp;
                     <span>  Users</span>
-                </a>
+                </a> -->
 
 
             <!-- END USERS -->
 
             <!-- PROFILE -->
-                <div class="pt-2"></div>
-                <a href="<?php echo BASE_URL . '/cu-admin/profile.php' ?>" style="text-decoration: none;" class="text-white ps-2 side">
+                <!-- <div class="pt-2"></div>
+                <a href="<?php //echo BASE_URL . '/cu-admin/profile.php' ?>" style="text-decoration: none;" class="text-white ps-2 side">
                     <i class="bi bi-person"></i>&nbsp;
                     <span>  Profile</span>
                 </a>
-                </span>
+                </span> -->
 
             <!-- END PROFILE -->
 
             <!-- LOGOUT -->
-                <div class="pt-2"></div>
-                <a href="<?php echo BASE_URL . '/logout.php' ?>" style="text-decoration: none;" class="text-white ps-2 side">
+                <!-- <div class="pt-2"></div>
+                <a href="<?php //echo BASE_URL . '/logout.php' ?>" style="text-decoration: none;" class="text-white ps-2 side">
                     <i class="bi bi-box-arrow-right"></i>&nbsp;
                     <span>  Logout</span>
-                </a>
+                </a> -->
             <!-- END LOGOUT -->
-			</div>
+			<!-- </div>
 		</div>
-	</div>
+	</div> -->
 
 <!-- close -->
 
