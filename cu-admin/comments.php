3ad326f2e91a8ad5aeafac4636a69870462e1611
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-18 08:12:02 -0600

    update

--- cu-admin/comments.php
+++ cu-admin/comments.php
@@ -80,8 +80,7 @@ if (isset($_POST['unapprove'])) {
 
   <div class="mt-5"></div>
   <div class="row">
-    <div class="col"></div>
-    <div class="col-11">
+    <div class="col-10">
       <table class="table table-bordered" style="width: 99%;">
         <thead>
           <tr>
