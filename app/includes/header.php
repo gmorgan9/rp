5e7f8f3c1cb9ffeed27f75014ce24b5446ff6ebc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-19 17:46:44 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -22,7 +22,7 @@
             <a class="head-link" style="z-index:9999 !important;text-decoration: none; color: white; font-size: 14px;" href="#" class="menu-btn text-white">
                 <i class="bi bi-plus"></i>New
             </a>
-            <div class="menu-content" style="z-index:9999 !important; margin-left: 10px !important; width: 60% !important;">
+            <div class="menu-content" style="margin-left: 10px !important; width: 60% !important;">
                 <a class="links text-white" style="z-index:9999 !important;" href="<?php echo BASE_URL . '/cu-admin/add_posts.php' ?>">Post</a>
                 <a class="links text-white" style="z-index:9999 !important;" href="<?php echo BASE_URL . '/cu-admin/categories.php' ?>">Category</a>
                 <a class="links text-white" style="z-index:9999 !important;" href="<?php echo BASE_URL . '/cu-admin/add_users.php' ?>">User</a>
