76c901ef6f6136d6b7d06df3d9c948cc021d252c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-19 15:57:41 -0600

    update

--- cu-login.php
+++ cu-login.php
@@ -52,7 +52,7 @@ if(mysqli_num_rows($result) > 0){
    $error = '
    <div class="login_error">
    <strong>Error:</strong> 
-   The username '. $_POST['username'] .' is not registered on this site. Please try again.
+   The username <strong>'. $_POST['username'] .'</strong> is not registered on this site. Please try again.
    </div>
    ';
 }
