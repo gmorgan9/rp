dd243b3db65347b8682bbb6e0c781a62a754ade4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-19 11:03:02 -0600

    update

--- cu-admin/categories.php
+++ cu-admin/categories.php
@@ -93,7 +93,7 @@ if(isset($_POST['add-category'])){
 
 <!-- START ADD COMPANY (LEFT SIDE) -->
 <div class="d-flex">
-<div class="page-content" style="width: 32%; height: 175px !important; border-radius: 15px;">
+<div class="page-content" style="background-color: white; width: 32%; height: 175px !important; border-radius: 15px;">
     <form action="" method="post">
     <div class="section-header pt-2">
       <span class="text-muted pt-4" style="width: 95%;">New Category</span>
