c13f75a90cd33a3545aca52104fca0d6111f97b0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-19 17:02:07 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -71,7 +71,7 @@ if(isLoggedIn() == false){
               <div class="card-body">
                 <h5 class="card-title">Special title treatment</h5>
                 <div class="d-flex">
-                  <p class="card-text float-start">
+                  <p class="card-text float-start w-25">
                     With supporting text below as a natural lead-in to additional content.
                   </p>
                   <p class="card-text float-end">
