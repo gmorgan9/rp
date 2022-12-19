54367d11b833b670beb5842ae3e94b2c7c399888
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-19 17:28:34 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -70,14 +70,14 @@ if(isLoggedIn() == false){
                 </div>
                 <div class="card-body">
                   <div class="pt-2"></div>
-                  <div class="d-flex">
+                  <!-- <div class="d-flex"> -->
                     <p class="card-text float-start w-25 justify-content-center" style="margin-left: auto; margin-right: auto;">
                       <div style="width: 65px; height: 35px; background-color: green; border-radius: 100px; margin-left: -50px; margin-right: 50px;"></div>
                     </p>
                     <p class="card-text float-end" style="font-size: 14px;">
                     Your site's health is looking <strong>good</strong>, but there are still some things you can do to improve its performance and security.
                     </p>
-                  </div>
+                  <!-- </div> -->
                 </div>
               </div>
             <!-- end health -->
