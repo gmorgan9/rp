605b511cc49b854ad14d2cc692bb147690ef34cc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-19 17:08:59 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -72,7 +72,7 @@ if(isLoggedIn() == false){
                 <h5 class="card-title">Special title treatment</h5>
                 <div class="d-flex">
                   <p class="card-text float-start w-25 justify-content-center" style="margin-left: auto; margin-right: auto;">
-                    <div style="width: 35px; height: 25px; background-color: green; border-radius: 100px; margin-left: -35px; margin-right: 35px;"></div>
+                    <div style="width: 35px; height: 25px; background-color: green; border-radius: 100px; margin-left: -50px; margin-right: 50px;"></div>
                   </p>
                   <p class="card-text float-end">
                     With supporting text below as a natural lead-in to additional content.
