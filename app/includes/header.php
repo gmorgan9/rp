85c810e793d14acded3ee04056a6061f304689c7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-19 16:04:11 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -8,8 +8,22 @@
         <a style="text-decoration: none; color: white" href="/"><i class="bi bi-chat-right"></i>&nbsp;0</a>
         <div class="pe-2"></div>
         <a style="text-decoration: none; color: white" href="/"><i class="bi bi-plus"></i>New</a>
+        <!-- start dropdown -->
+        <div class="dropdown-menu">
+            <a style="text-decoration: none; color: white" href="#" class="menu-btn text-white">
+                <i class="bi bi-plus"></i>New
+            </a>
+            <div class="menu-content">
+                <a class="links text-white" href="<?php echo BASE_URL . '/cu-admin/profile.php' ?>">Edit Profile</a>
+                <!-- <a class="links text-white" href="#">Visit Us</a> -->
+                <a class="links text-white" href="<?php echo BASE_URL . '/logout.php' ?>">Comment</a>
+                <div class="pb-3"></div>
+            </div>
+        </div>
+        <!-- end dropdown -->
     </div>
     <div style="float: right !important;">
+    <!-- start dropdown -->
         <div class="dropdown-menu">
             <a style="font-size: 14px; text-decoration: none; color: white" href="/" class="menu-btn text-white">
                 Welcome, <?php echo $_SESSION['username']; ?> <i class="bi bi-person-square"></i> 
@@ -26,5 +40,6 @@
                 </div>
             </div>
         </div>
+        <!-- end dropdown -->
     </div>
 </div>
