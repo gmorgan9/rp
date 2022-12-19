7b57ceb8f6fef997fa00abfd53b778c98f824490
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-19 09:54:37 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -112,8 +112,8 @@ if (mysqli_num_rows($result) > 0) {
 
                                 <div class="nav__dropdown-collapse">
                                     <div class="nav__dropdown-content">
-                                        <a href="<?php echo BASE_URL . '/pages/add_posts.php' ?>" class="nav__dropdown-item side text-white">&nbsp;Add Posts</a>
-                                        <a href="<?php echo BASE_URL . '/pages/categories.php' ?>" class="nav__dropdown-item side text-white">&nbsp;Categories</a>
+                                        <a href="<?php echo BASE_URL . '/pages/add_posts.php' ?>" class="nav__dropdown-item side text-muted">&nbsp;Add Posts</a>
+                                        <a href="<?php echo BASE_URL . '/pages/categories.php' ?>" class="nav__dropdown-item side text-muted">&nbsp;Categories</a>
                                     </div>
                                 </div>
                             </div>
