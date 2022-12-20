e0600aebb3bad4d3c539b3b15a588db01de1d284
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 14:15:02 -0600

    update

--- index.php
+++ index.php
@@ -48,6 +48,7 @@ if(isset($_POST['register'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
