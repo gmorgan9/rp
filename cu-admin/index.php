4a6884234650e70d6c4f7b3b7be5272c30dd9282
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 08:21:57 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -273,7 +273,7 @@ if(isLoggedIn() == false){
 
     <div class="row">
       <div class="col-2"></div>
-      <div class="col position-absolute bottom-0">
+      <div class="col position-absolute bottom-0 ps-5">
         <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
       </div>
     </div>
