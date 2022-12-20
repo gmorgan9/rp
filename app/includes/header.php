1e06a679556d07681d07dc066078a0b0f12581ca
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 10:40:28 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -33,7 +33,7 @@
     </div>
     <div style="float: right !important;">
     <!-- start dropdown -->
-        <div class="dropdown-menu">
+        <div class="dropdown-menu" style="z-index: 1;">
             <a class="head-link" style="font-size: 14px; text-decoration: none; color: white" href="/" class="menu-btn text-white">
                 Welcome, <?php echo $_SESSION['username']; ?> <i class="bi bi-person-square"></i> 
             </a>
