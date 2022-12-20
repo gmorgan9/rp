4f9dce115e4eff879fc450eef9c1080f78463cd5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 12:21:19 -0600

    update

--- cu-admin/edit_post.php
+++ cu-admin/edit_post.php
@@ -206,12 +206,6 @@ if (mysqli_num_rows($result) > 0) {
       selector: 'textarea#content',
       plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
       toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-      tinycomments_mode: 'embedded',
-      tinycomments_author: 'Author name',
-      mergetags_list: [
-        { value: 'First.Name', title: 'First Name' },
-        { value: 'Email', title: 'Email' },
-      ]
     });
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script type="text/javascript">
