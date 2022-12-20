2a5edcf3aefe1167247eb6eb0b889a0bc50c74c0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 14:14:44 -0600

    update

--- index.php
+++ index.php
@@ -55,6 +55,7 @@ if(isset($_POST['register'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="icon" type="image/x-icon" href="<?php echo ROOT_PATH . '/assets/images/favicon.ico'; ?>">
 
 
     <link rel="stylesheet" href="assets/blog.css?v=3.92">
