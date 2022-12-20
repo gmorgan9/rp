c22c3f838c34cae72c006dc2a447d15de36ebe03
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 08:14:49 -0600

    update

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,5 +1,5 @@
 <!-- WORKING -->
-<footer>
+<footer style="margin: 0; padding: 0;">
     <div class="text-center text-muted pt-1 copyright">
         <?php $year = date("Y"); ?>
         &copy; <?php echo $year; ?> CacheUp
