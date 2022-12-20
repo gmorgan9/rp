ee8c43c6ea4942c787dd87714d13ebed064f3a80
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 10:23:54 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -245,7 +245,7 @@ if(isLoggedIn() == false){
                 Quick Draft
               </div>
               <div class="card-body">
-                <form action="" method="POST" style="z-index: 9999 !important;">
+                <form action="" method="POST" style="z-index: -1;">
                   <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
                     <input type="text" class="form-control" id="title" name="title">
