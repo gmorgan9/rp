e39a76ea18f93b0e47bd32cec7e442f332adfae1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 10:21:41 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -245,13 +245,7 @@ if(isLoggedIn() == false){
                 Quick Draft
               </div>
               <div class="card-body">
-                <form action="" method="POST">
-                <div class="mb-4 row">
-                            <label for="password" class="col-sm-1 col-form-label"><div class="d-flex">Password&nbsp;<span style="color: red;">*</span></div></label>
-                            <div class="col-sm-10">
-                                <input type="password" class="form-control w-50" id="password" name="password" required>
-                            </div>
-                        </div>
+                <form action="" method="POST" style="z-index: -1;">
                   <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
                     <input type="text" class="form-control" id="title" name="title">
