0d242b28cf947ec903f8b26e1c1dd776b5c821eb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 09:04:32 -0600

    update

--- cu-admin/add_user.php
+++ cu-admin/add_user.php
@@ -247,7 +247,8 @@ if(isset($_POST['update'])){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 
-    <link rel="stylesheet" href="../assets/styles.css?v=2.50">
+    <link rel="stylesheet" href="../assets/styles.css?v=4.01">
+    <link rel="stylesheet" href="../assets/sidebar.css?v=1.10">
 
     <script src="https://cdn.tiny.cloud/1/7kainuaawjddfzf3pj7t2fm3qdjgq5smjfjtsw3l4kqfd1h4/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
@@ -383,14 +384,7 @@ if (mysqli_num_rows($result) > 0) {
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script type="text/javascript">
-  $(document).on('click', 'a', function() {
-    $(this).addClass('active').siblings().removeClass('active')
-  })
-  $(document).on('click', 'ul li a', function() {
-    $(this).removeClass('active')
-  })
-</script>
+
     <script src="../assets/js/dropdown.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
