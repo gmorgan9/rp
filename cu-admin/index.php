039ff6d4b89d8120ebcf84358a9af87056283b54
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 15:19:11 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -15,6 +15,7 @@ if(isLoggedIn() == false){
 <html lang="en">
 <head>
     <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
