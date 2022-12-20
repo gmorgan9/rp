794972d719abc72040a8d76febcec1ad76a3b3aa
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 10:03:52 -0600

    update

--- cu-admin/profile.php
+++ cu-admin/profile.php
@@ -171,13 +171,16 @@ if (mysqli_num_rows($result) > 0) {
                 </div>
                 
               </div>
-              <a href="edit_account.php?id=<?php echo $user_id; ?>" class="btn btn-outline-success">
-                Edit Account
-              </a>
-              <form method="post" action="">
-                <input type="hidden" name="user_id" value="<?php echo $user_id; ?>" />
-                <button onclick="return confirm('Password Reset? \r\nAn email is on its way!')" type="submit" name="reset" class="btn btn-outline-secondary">Reset Password</button>
-              </form>
+              <div class="d-flex">
+                <a href="edit_account.php?id=<?php echo $user_id; ?>" class="btn btn-outline-success">
+                  Edit Account
+                </a>
+                &nbsp;
+                <form method="post" action="">
+                  <input type="hidden" name="user_id" value="<?php echo $user_id; ?>" />
+                  <button onclick="return confirm('Password Reset? \r\nAn email is on its way!')" type="submit" name="reset" class="btn btn-outline-secondary">Reset Password</button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
