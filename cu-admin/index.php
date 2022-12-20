41cd2a31b3ba7d885ccd3c2ecd0f40c5e139a302
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 07:48:11 -0600

    update

--- cu-admin/index.php
+++ cu-admin/index.php
@@ -181,7 +181,7 @@ if(isLoggedIn() == false){
                     Recent Comments
                   </p>
                   <?php foreach ($comms as $comm) { ?>
-                    <div class="row" style="background-color: #f0f0f0;">
+                    <div class="row">
                       <div class="col-2" style="margin-top: -15px !important;">
                         <i style="font-size: 55px; margin-left: 15px;" class="bi bi-person-square text-muted"></i>
                       </div>
@@ -195,7 +195,7 @@ if(isLoggedIn() == false){
                       </div>
                     </div>
                   <?php } ?>
-                  <hr>
+                  <hr style="width: 100%;">
 
                 </div>
               </div>
