9d83b18cac6d9b651f3dae9a8854fc9a83c0c313
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-20 12:21:37 -0600

    update

--- cu-admin/edit_post.php
+++ cu-admin/edit_post.php
@@ -200,22 +200,21 @@ if (mysqli_num_rows($result) > 0) {
 
 
 
-
 <script>
     tinymce.init({
       selector: 'textarea#content',
       plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
       toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+      tinycomments_mode: 'embedded',
+      tinycomments_author: 'Author name',
+      mergetags_list: [
+        { value: 'First.Name', title: 'First Name' },
+        { value: 'Email', title: 'Email' },
+      ]
     });
+  </script>
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script type="text/javascript">
-  $(document).on('click', 'a', function() {
-    $(this).addClass('active').siblings().removeClass('active')
-  })
-  $(document).on('click', 'ul li a', function() {
-    $(this).removeClass('active')
-  })
-</script>
+<script src="../assets/js/bar.js"></script>
     <script src="../assets/js/dropdown.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
