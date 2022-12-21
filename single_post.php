291df90b8eea13a88f4bf59f61ddb7aa8be9f6aa
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 10:42:30 -0600

    update

--- single_post.php
+++ single_post.php
@@ -466,7 +466,7 @@ if (mysqli_num_rows($result) > 0) {
   <!-- sidebar -->
     
     <div class="side" id="side" style="float: right; position: sticky; top: 0; right: 0; margin-top: 0; margin-right: 0 !important; padding-right: 0 !important; background-color: #1f1f1f; width: 30%;">  
-      <div class="side-content" id="side_content" style="margin-top: 35px; position: sticky; padding-left: 2%; padding-top: 2%;">
+      <div class="side-content" id="side_content" style="margin-top: 45px; position: sticky; padding-left: 2%; padding-top: 2%;">
       <!-- recent posts -->
         <h4>
           Recent Posts
