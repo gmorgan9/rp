ef44b9b22fe526d5eadf16594dd0fbd5c0098744
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 09:21:01 -0600

    update

--- single_post.php
+++ single_post.php
@@ -459,7 +459,7 @@ if (mysqli_num_rows($result) > 0) {
   
   </div>
   
-  <div class="side" style="position: sticky; float: right; margin-top: 0; margin-right: 0 !important; padding-right: 0 !important; background-color: #1f1f1f; width: 30%; height: inherit;">  
+  <div class="side" style="position: sticky; float: right; margin-top: 0; margin-right: 0 !important; padding-right: 0 !important; background-color: #1f1f1f; width: 30%; height: inherit !important;">  
     <h4>
       happy
     </h4>
