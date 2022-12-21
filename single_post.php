b3d62e629707264355be3617d8ebf08c86baa363
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 10:39:53 -0600

    update

--- single_post.php
+++ single_post.php
@@ -577,7 +577,7 @@ $(document).ready(function () {
             // var el = $('#side_content'); // important! (local)
             var elpos = el.offset().top; // take current situation
             var windowpos = $(window).scrollTop();
-            var finaldestination = topEl + elpos ;
+            var finaldestination = windowpos - elpos + topEl;
             el.stop().animate({ 'top': finaldestination });
         });
     });
