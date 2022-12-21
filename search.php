cb29ab12aa985009becf01e63e1e5b802d86c093
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 11:09:36 -0600

    update

--- search.php
+++ search.php
@@ -132,7 +132,7 @@ if (mysqli_num_rows($result) > 0) {
         $results = mysqli_query($conn, $select);
         if (mysqli_num_rows($results) > 0) {
             while($row = mysqli_fetch_assoc($results)) {
-                $username = $row['username'];
+                $new = $row['username'];
 
 
             }}
@@ -151,7 +151,7 @@ if (mysqli_num_rows($result) > 0) {
 
 
 <p>
-    <?php echo $results; ?>
+    <?php echo $new; ?>
 </p>
 
 
