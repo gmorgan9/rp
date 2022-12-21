a91a7ff402e0dc570ce31909855511627d3f6d35
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 09:49:06 -0600

    update

--- single_post.php
+++ single_post.php
@@ -510,9 +510,7 @@ if (mysqli_num_rows($result) > 0) {
 <!-- Other Scripts and Footer -->
   <script src="assets/js/dropdown.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  <div id="footer">
-    <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
-  </div>
+  <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
 <!-- END Other Scripts and Footer -->
 
 <script>
@@ -520,8 +518,6 @@ if (mysqli_num_rows($result) > 0) {
   elemHgt = divElem.offsetHeight + 48;
   const side = document.querySelector('#side');
   side.style.height = elemHgt + "px";
-  const footer = document.querySelector('#footer');
-  footer.style.margin-top = elemHgt + "px";
 </script>
 </body>
 </html>
