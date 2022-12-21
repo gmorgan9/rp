771ecf467a79d4fabce5d96a98f87b154cf3d974
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 13:13:02 -0600

    update

--- search.php
+++ search.php
@@ -134,6 +134,14 @@ if (mysqli_num_rows($result) > 0) {
     <div class="mt-5"></div>
     <div class="d-flex justify-content-center">
       <form action="" method="post" class="">
+        
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">@</span>
+        </div>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+      </div>
+
         <div class="input-group">
         <div class="input-group-prepend">
           <button type="submit" name="search" class="input-group-text"><i class="bi bi-search"></i></button>
