3efea4132808fb4ab0a084fbf3127cec399b2817
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 14:07:39 -0600

    update

--- search.php
+++ search.php
@@ -157,6 +157,16 @@ if (mysqli_num_rows($result) > 0) {
     if(isset($_POST['search'])){
         $param = mysqli_real_escape_string($conn, $_POST['param']);
         $select = " SELECT * FROM posts WHERE title LIKE '%$param%'";
+        $results = mysqli_query($conn, $select);
+        if(mysqli_num_rows($results) < 0){
+          $error = '
+            <div class="pt-3"></div>
+            <div class="login_error">
+            <strong>Error:</strong> 
+            The username <strong>'. $_POST['username'] .'</strong> or password entered is not registered on this site. Please try again.
+            </div>
+            ';
+        }
     };
     ?>
 <?php echo $error; ?>
@@ -172,7 +182,7 @@ if (mysqli_num_rows($result) > 0) {
 
     <div class="mx-auto pop-post row row-cols-1 row-cols-md-3 g-4">
     <?php 
-    $results = mysqli_query($conn, $select);
+    
       if(mysqli_num_rows($results) > 0){
         while($row = mysqli_fetch_assoc($results)) {
             $title = $row['title'];
