8c81322645d25679827b7a37d0082e060311ee66
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 14:11:00 -0600

    update

--- search.php
+++ search.php
@@ -175,7 +175,7 @@ if (mysqli_num_rows($result) > 0) {
             <div class="pt-3"></div>
             <div class="login_error">
             <strong>Error:</strong> 
-            The username <strong>'. $_POST['username'] .'</strong> or password entered is not registered on this site. Please try again.
+            Please try again.
             </div>
             ';
         }else {
