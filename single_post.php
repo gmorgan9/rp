b7cf933ea9fb980e4668dbcd3f344fb4fd934bde
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 08:35:00 -0600

    update

--- single_post.php
+++ single_post.php
@@ -459,7 +459,7 @@ if (mysqli_num_rows($result) > 0) {
 
 
   </div>
-  <div class="side" style="margin-left: 65%;">
+  <div class="side" style="margin-left: 85%;">
     <h4>
       happy
     </h4>
