bf7cd169860d4c84c5c12c5647da8c7c76e288fb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 10:43:51 -0600

    update

--- single_post.php
+++ single_post.php
@@ -574,8 +574,8 @@ if (mysqli_num_rows($result) > 0) {
         $(window).scroll(function () {
             var elpos = el.offset().top; // take current situation
             var windowpos = $(window).scrollTop();
-            var finaldestination = windowpos - elpos +45;
-            el.stop().animate({ 'top': finaldestination });
+            var finaldestination = windowpos - elpos;
+            el.stop().animate({ 'top': finaldestination +45 });
         });
     });
 </script>
