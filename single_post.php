9721519cc67d1d18a501a9af826350638cd2fef6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 08:55:58 -0600

    update

--- single_post.php
+++ single_post.php
@@ -188,7 +188,7 @@ if (mysqli_num_rows($result) > 0) {
 <div class="main">
 
 <!-- start header -->
-  <div class="page-header">
+  <div class="page-header position-fixed">
     <div class="left">
       <a href="/">
         <img src="/assets/images/white-logo.png" width="230px" class="text-center" style="margin-top: 2.5%; margin-left: 2%;" alt="">
