65c31f3036a57ad7b4367d576d0dc621ba9f25c9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 14:29:22 -0600

    update

--- search.php
+++ search.php
@@ -1,16 +1,12 @@
-<!-- top PHP -->
-  <?php
-  require_once "app/database/connection.php";
-  require_once "path.php";
-  session_start();
-  ?>
-<!-- end top PHP -->
+<?php
+require_once "app/database/connection.php";
+require_once "path.php";
+session_start();
+?>
 
-
-<!-- open tags -->
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -59,10 +55,8 @@
         display: none;
       }
     </style>
-  </head>
-<!-- end open tags -->
-<!-- body -->
-  <body>
+</head>
+<body>
   <?php
   $user_id = $_SESSION['user_id'];
   $select = " SELECT * FROM users WHERE user_id = '$user_id' ";
@@ -160,6 +154,9 @@
         <div class="card-body mb-4">
           <p class="card-subtitle mb-3 mt-4 text-uppercase fw-bold" style="font-size: 12px;color: #03c6fc;"><?php echo $s['category']; ?></p>
           <a href="single_post.php?id=<?php echo $s['post_id']; ?>" class="text-decoration-none text-white"><h5 class="card-title blog-title"><?php echo $s['title']; ?></h5></a>
+          <p>
+            <?php echo $s['author']; ?> / <?php echo $s['published_at']; ?>
+          </p>
         </div>
       </div>
     </div>
@@ -207,6 +204,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
     <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
-  </body>
-<!-- end body -->
+</body>
 </html>
