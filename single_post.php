5987f7c8a0a98bb1c63913add5c867a72b15c0c1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-21 09:15:10 -0600

    update

--- single_post.php
+++ single_post.php
@@ -509,7 +509,7 @@ if (mysqli_num_rows($result) > 0) {
 <!-- Other Scripts and Footer -->
   <script src="assets/js/dropdown.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+  <?php //include(ROOT_PATH . "/app/includes/footer.php"); ?>
 <!-- END Other Scripts and Footer -->
 
 </body>
