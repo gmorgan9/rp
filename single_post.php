a508a0ce92dcdd4eb76e6295984352e494f356da
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-22 17:30:22 -0600

    update

--- single_post.php
+++ single_post.php
@@ -604,7 +604,7 @@ if (mysqli_num_rows($result) > 0) {
 
         //run on scroll
         $(window).scroll(function () {
-            var elpos = el.offset().top; // take current situation
+            var elpos = el.offset().top + 45; // take current situation
             var windowpos = $(window).scrollTop();
             var finaldestination = windowpos - elpos;
             el.stop().animate({ 'top': finaldestination }, 1000);
