7d502c1cb242d4e73402273c90324779719bb041
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-22 09:59:09 -0600

    update

--- author_page.php
+++ author_page.php
@@ -24,6 +24,12 @@ session_start();
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <style>
+        .mx-auto {
+ margin-right: auto !important;
+ margin-left: auto !important;
+}
+    </style>
 </head>
 <body>
 <?php
