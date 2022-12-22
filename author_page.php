bdf6ab0b51eaebeaf77f5dd034ac46958956c305
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-22 09:58:29 -0600

    update

--- author_page.php
+++ author_page.php
@@ -80,7 +80,7 @@ if (mysqli_num_rows($result) > 0) {
 
 
 
-    <div class="pt-5 bio d-flex justify-content-center">
+    <div class="pt-5 bio mx-auto">
         <img src="<?php echo $pp; ?>" style="width: 150px; border-radius: 100%; border: 1px solid #393939;" class="img-fluid" alt="">
         <h5>
             <?php echo $firstname; ?> <?php echo $lastname; ?>
