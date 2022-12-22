f638a87dfa7c0c8ae805cdd525c9c84ae5c2a6ab
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-22 17:00:14 -0600

    update

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ session_start();
   border: none;
   background-color: transparent;
   /* position: relative; */
-  /* display: inline-block; */
+  display: block;
 }
 .menu-content {
   /* margin-left: -25% !important; */
