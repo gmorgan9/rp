3159694062525254e71f05c41d37e2d38c2f55f3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-22 10:17:18 -0600

    update

--- author_page.php
+++ author_page.php
@@ -92,7 +92,7 @@ if (mysqli_num_rows($result) > 0) {
         <p class="text-center text-muted">
             <?php echo $username; ?>
         </p>
-        <p class="text-uppercase text-muted text-center" style="font-size: 12px;">
+        <p class="text-uppercase text-muted text-center" style="font-size: 12px; letter-spacing: .5px;">
             <?php 
             ?>
             Joined: <?php echo date('F d, Y', strtotime($joined)); ?>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;Posts: 
