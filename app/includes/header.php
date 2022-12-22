772de96a2336db75c4463f0c60d8f4f9b7d0a3b5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-22 17:13:56 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -6,7 +6,7 @@
                 <div class="pe-4"></div>
                 <a class="head-link" style="text-decoration: none; color: white; font-size: 12px;" href="/"><i class="bi bi-house-door-fill"></i>&nbsp;CacheUp</a>
                 <div class="pe-5"></div>
-                &nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;
                 <a class="head-link" style="text-decoration: none; color: white; font-size: 12px;" href="<?php echo BASE_URL . '/cu-admin/pending_comments.php' ?>"><i style="font-size: 12px;" class="bi bi-chat-right-fill"></i>&nbsp;
 
                 <?php
