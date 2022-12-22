8283ee5b626533a997fc6c3d434b6075cc38034c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-22 17:36:55 -0600

    update

--- single_post.php
+++ single_post.php
@@ -598,18 +598,18 @@ if (mysqli_num_rows($result) > 0) {
 
 <!-- scroll script -->
   <script>
-  $(document).ready(function () {
-     var el = $('#side_content');
-        var originalelpos = el.offset().top; // take it where it originally is on the page
+  // $(document).ready(function () {
+  //    var el = $('#side_content');
+  //       var originalelpos = el.offset().top; // take it where it originally is on the page
 
-        //run on scroll
-        $(window).scroll(function () {
-            var elpos = el.offset().top; // take current situation
-            var windowpos = $(window).scrollTop();
-            var finaldestination = elpos + originalelpos;
-            el.stop().animate({ 'top': finaldestination }, 1000);
-        });
-    });
+  //       //run on scroll
+  //       $(window).scroll(function () {
+  //           var elpos = el.offset().top; // take current situation
+  //           var windowpos = $(window).scrollTop();
+  //           var finaldestination = elpos + originalelpos;
+  //           el.stop().animate({ 'top': finaldestination }, 1000);
+  //       });
+  //   });
   </script>
 <!-- end scroll script -->
 <!-- adds height -->
