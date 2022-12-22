d2bc4dbdc9311a25c56a7c907fa4f0d450dfc722
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-22 16:10:43 -0600

    update

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if($loggedin == 1) { ?>
   
   <div class="row" style="width: 100%; background-color: #1e2327; height: 30px;">
     <p class="col-10"><a href="<?php echo BASE_URL . '/cu-admin/' ?>">CacheUp</a></p>
-      <p class="pe-2 pt-2 col" style="font-size: 12px;">Welcome, <?php echo $_SESSION['username']; ?></p> 
+    <p class="pt-2 col" style="font-size: 12px;">Welcome, <?php echo $_SESSION['username']; ?></p> 
   </div>
 
 <?php } else {}
