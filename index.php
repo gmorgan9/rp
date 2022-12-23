52d1906ec9d7055aab960205ca30870658156fbd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-22 22:37:19 -0600

    update

--- index.php
+++ index.php
@@ -253,18 +253,20 @@ if (mysqli_num_rows($result) > 0) {
       $options= mysqli_fetch_all($result, MYSQLI_ASSOC);
     }
 
-    
-
-  ?>
-  <?php foreach ($options as $option) { 
-
-      $category = $option['category'];
+    $category = $options['category'];
       $select = " SELECT * FROM category WHERE idno = '$category' ";
       $result = mysqli_query($conn, $select);
       if (mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_assoc($result)) {
           $name    = $row['category'];
       }}
+
+    
+
+  ?>
+  <?php foreach ($options as $option) { 
+
+      
     
     ?>
     
