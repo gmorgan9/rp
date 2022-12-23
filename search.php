b440411877b6f9d7ddcc228e8818351cb144fcb1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-22 21:39:44 -0600

    update

--- search.php
+++ search.php
@@ -19,7 +19,7 @@ session_start();
     <link rel="icon" type="image/x-icon" href="<?php echo BASE_URL . '/assets/images/favicon.ico'; ?>">
 
 
-    <link rel="stylesheet" href="assets/blog.css?v=4.00">
+    <link rel="stylesheet" href="assets/blog.css?v=4.01">
 
     <title>Search - CacheUp Blog</title>
 
