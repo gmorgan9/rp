60906130e33749862c758c2d7f615d074f3970b6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-22 22:46:20 -0600

    update

--- search.php
+++ search.php
@@ -259,7 +259,7 @@ if($loggedin == 1) { ?>
     <div class="col">
       <div class="card h-100" style="background-color: #1f1f1f;">
         <div class="card-body mb-4">
-          <p class="card-subtitle mb-3 mt-4 text-uppercase fw-bold" style="font-size: 12px;color: #03c6fc;"><?php echo $s['category']; ?></p>
+          <p class="card-subtitle mb-3 mt-4 text-uppercase fw-bold" style="font-size: 12px;color: #03c6fc;"><?php echo $name; ?></p>
           <a href="single_post.php?id=<?php echo $s['post_id']; ?>" class="text-decoration-none text-white"><h5 class="card-title blog-title"><?php echo $s['title']; ?></h5></a>
           <div class="pt-4"></div>
           <p class="text-muted" style="font-size: 14px; margin-bottom: -10px;">
