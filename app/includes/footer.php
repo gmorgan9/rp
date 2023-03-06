0fd196d4f8337f2033d3575cc145001174712c68
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-03-05 18:17:47 -0700

    update

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -2,6 +2,6 @@
 <footer>
     <div class="text-center text-muted pt-1 copyright">
         <?php $year = date("Y"); ?>
-        &copy; <?php echo $year; ?> CacheUp
+        &copy; <?php echo $year; ?> RecipePro
     </div>
 </footer>
