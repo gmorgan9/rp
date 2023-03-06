b5332daacf7d1a366005c78dd5dc11fdea09b47e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-03-05 20:16:37 -0700

    update

--- index.php
+++ index.php
@@ -272,7 +272,7 @@ if (mysqli_num_rows($result) > 0) {
       <div class="card h-100" style="background-color: #1f1f1f;">
         <div class="card-body mb-4">
           <p class="card-subtitle mb-3 mt-4 text-uppercase fw-bold" style="font-size: 12px;color: #03c6fc;"><?php echo $name; ?></p>
-          <a href="single_recipe.php?id=<?php echo $option['post_id']; ?>" class="text-decoration-none text-white"><h5 class="card-title blog-title"><?php echo $option['title']; ?></h5></a>
+          <a href="single_recipe.php?id=<?php echo $option['title']; ?>" class="text-decoration-none text-white"><h5 class="card-title blog-title"><?php echo $option['title']; ?></h5></a>
           <!-- <p class="card-text text-muted">
             <?php //echo html_entity_decode(substr($option['content'], 0, 150) . '...'); ?>
           </p> -->
